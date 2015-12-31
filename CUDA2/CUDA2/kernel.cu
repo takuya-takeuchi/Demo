c5d1595b547e31ee1542a99a55557b6fcb6a1e0c
takuya-takeuchi <spitzbergen@gmail.com>
2015-12-31 23:41:36 +0900

    Forget to commit

---
+++ CUDA2/CUDA2/kernel.cu
@@ -0,0 +1,165 @@
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <chrono>
+#include <stdio.h>
+
+const int g_loop = 100000000;
+
+cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+
+__global__ void addKernel(int *c, const int *a, const int *b, const int loop)
+{
+	int i = threadIdx.x;
+	for (unsigned int t = 0; t < loop; ++t)
+	{
+		c[i] = a[i] + b[i];
+	}
+}
+
+int main()
+{
+	const int arraySize = 1024;
+	int a[arraySize] = { 0 };
+	int b[arraySize] = { 0 };
+	int c[arraySize] = { 0 };
+	for (size_t i = 0; i < arraySize; i++)
+	{
+		a[i] = i + 1;
+		b[i] = (i + 1) * 10;
+		c[i] = 0;
+	}
+
+	// Add vectors in parallel.
+	cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "addWithCuda failed!");
+		return 1;
+	}
+
+	/*std::chrono::high_resolution_clock::time_point start, stop;
+
+	start = std::chrono::high_resolution_clock::now();
+
+	for (size_t i = 0; i < g_loop; i++)
+	{
+		auto pA = a;
+		auto pB = b;
+		auto pC = c;
+
+		for (size_t j = 0; j < arraySize; j++, pA++, pB++, pC++)
+		{
+			*pC = *pA + *pB;
+		}
+	}
+	stop = std::chrono::high_resolution_clock::now();
+
+	printf("No CUDA is\n");
+	printf("\t{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
+		c[0], c[1], c[2], c[3], c[4]);
+
+	auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+	printf("\ttime = {%lld}\n", ms.count());*/
+
+	// cudaDeviceReset must be called before exiting in order for profiling and
+	// tracing tools such as Nsight and Visual Profiler to show complete traces.
+	cudaStatus = cudaDeviceReset();
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaDeviceReset failed!");
+		return 1;
+	}
+
+	return 0;
+}
+
+// Helper function for using CUDA to add vectors in parallel.
+cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
+{
+	int *dev_a = 0;
+	int *dev_b = 0;
+	int *dev_c = 0;
+	cudaError_t cudaStatus;
+
+	// Choose which GPU to run on, change this on a multi-GPU system.
+	cudaStatus = cudaSetDevice(0);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+		goto Error;
+	}
+
+	// Allocate GPU buffers for three vectors (two input, one output)    .
+	cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed!");
+		goto Error;
+	}
+
+	cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed!");
+		goto Error;
+	}
+
+	cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed!");
+		goto Error;
+	}
+
+	// Copy input vectors from host memory to GPU buffers.
+	cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy failed!");
+		goto Error;
+	}
+
+	cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy failed!");
+		goto Error;
+	}
+
+	std::chrono::high_resolution_clock::time_point start, stop;
+	start = std::chrono::high_resolution_clock::now();
+
+	// Launch a kernel on the GPU with one thread for each element.
+	addKernel << <1, size >> >(dev_c, dev_a, dev_b, g_loop);
+
+	// Check for any errors launching the kernel
+	cudaStatus = cudaGetLastError();
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+		goto Error;
+	}
+
+	// cudaDeviceSynchronize waits for the kernel to finish, and returns
+	// any errors encountered during the launch.
+	cudaStatus = cudaDeviceSynchronize();
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+		goto Error;
+	}
+
+	stop = std::chrono::high_resolution_clock::now();
+
+	// Copy output vector from GPU buffer to host memory.
+	cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy failed!");
+		goto Error;
+	}
+
+	printf("CUDA is\n");
+	printf("\t{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
+		c[0], c[1], c[2], c[3], c[4]);
+
+	auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+	printf("\ttime = {%lld}\n", ms.count());
+
+Error:
+	cudaFree(dev_c);
+	cudaFree(dev_a);
+	cudaFree(dev_b);
+
+	return cudaStatus;
+}
