ee336c91f1c7926b98ea58ef9baa466be2123d90
takuya-takeuchi <spitzbergen@gmail.com>
2016-04-24 02:24:06 +0900

    初回コミット

---
+++ Xamarin.Forms.Portable2/Xamarin.Forms.Portable2/Xamarin.Forms.Portable2/Converters/BooleanInvertConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Xamarin.Forms.Portable2.Converters
+{
+    public sealed class BooleanInvertConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !((bool) value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
