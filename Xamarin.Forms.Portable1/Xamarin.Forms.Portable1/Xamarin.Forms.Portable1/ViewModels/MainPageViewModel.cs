397b2a09f2e355da28bfd10ddc5bb47818782629
takuya-takeuchi <spitzbergen@gmail.com>
2016-04-23 21:57:05 +0900

    初回コミット

---
+++ Xamarin.Forms.Portable1/Xamarin.Forms.Portable1/Xamarin.Forms.Portable1/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Practices.Prism.Mvvm;
+
+namespace Xamarin.Forms.Portable1.ViewModels
+{
+    public sealed class MainPageViewModel : BindableBase
+    {
+
+        public MainPageViewModel()
+        {
+            this.MainText = "Hello world from MVVM!!";
+        }
+
+        private string _MainText;
+
+        public string MainText
+        {
+            get
+            {
+                return this._MainText;
+            }
+            set
+            {
+                this._MainText = value;
+                this.SetProperty(ref this._MainText, value);
+            }
+        }
+    }
+}
