29f79b5b9064b94b0286d6a1c9a221734d38d250
takuya-takeuchi <spitzbergen@gmail.com>
2016-04-26 01:00:52 +0900

    Initial Commit

---
+++ Xamarin.Forms.Portable3/Xamarin.Forms.Portable3/Xamarin.Forms.Portable3/Views/Pages/StopwatchPageView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Xamarin.Forms.Portable3.Views.Pages
+{
+    public partial class StopwatchPageView : ContentPage
+    {
+        public StopwatchPageView()
+        {
+            InitializeComponent();
+        }
+    }
+}
