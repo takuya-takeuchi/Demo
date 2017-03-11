6bf02af2ad5f2fbbeddb8a4c478917ae9eec077d
takuya-takeuchi <spitzbergen@gmail.com>
2017-03-12 01:28:29 +0900

    Initial Commit

---
+++ WPF.Python3/WPF/ViewModels/Interfaces/IPythonWrapper.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace WPFPython.ViewModels.Interfaces
+{
+
+    public interface IPythonWrapper
+    {
+
+        Dictionary<string, object> load(string path);
+
+    }
+
+}
---
+++ WPF.Python3/WPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using WPFPython.ViewModels.Interfaces;
+
+namespace WPFPython.ViewModels
+{
+
+    public sealed class MainViewModel : ViewModelBase, IMainViewModel
+    {
+
+        #region フィールド
+
+        private readonly IPythonWrapper _PythonWrapper;
+
+        private readonly IMessageDialog _MessageDialog;
+
+        #endregion
+
+        #region コンストラクタ
+
+        public MainViewModel(IPythonWrapper pythonWrapper, IMessageDialog messageDialog)
+        {
+            if (pythonWrapper == null)
+                throw new ArgumentNullException(nameof(pythonWrapper));
+            if (messageDialog == null)
+                throw new ArgumentNullException(nameof(messageDialog));
+
+            this._PythonWrapper = pythonWrapper;
+            this._MessageDialog = messageDialog;
+
+            var keys = new string[]
+            {
+                "name",
+                "birth",
+                "dead",
+                "nation",
+                "publish",
+            };
+
+            this._Keys = new ObservableCollection<string>(keys);
+        }
+
+        #endregion
+
+        #region プロパティ
+
+        private RelayCommand _MessageCommand;
+
+        public RelayCommand MessageCommand
+        {
+            get
+            {
+                return this._MessageCommand ?? new RelayCommand(() =>
+                {
+                    var dict = this._PythonWrapper.load("sample.json");
+
+                    //var message = $"Initial total:{sum}, final total:{list.Sum()}";
+                    //this._MessageDialog.ShowMessage(message);
+                });
+            }
+        }
+
+        private string _SelectedKey;
+
+        public string SelectedKey
+        {
+            get
+            {
+                return this._SelectedKey;
+            }
+            set
+            {
+                this._SelectedKey = value;
+                this.RaisePropertyChanged();
+            }
+        }
+
+        private ObservableCollection<string> _Keys;
+
+        public ObservableCollection<string> Keys
+        {
+            get
+            {
+                return this._Keys;
+            }
+        }
+
+        #endregion
+
+    }
+}
