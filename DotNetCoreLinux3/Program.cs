9fc8f4308691b2b26000629a271e0662b2144085
Takuya Takeuchi <spitzbergen@gmail.com>
2017-05-26 00:42:17 +0900

    Initial Commit

---
+++ DotNetCoreLinux3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"Argument is '{args[0]}'");
+
+            var path = System.IO.Path.GetFullPath(args[0]);
+            Console.WriteLine($"Path is '{path}'");
+
+            foreach(var f in new System.IO.DirectoryInfo(path).GetFiles())
+                Console.WriteLine($"{f.Name}");
+        }
+    }
+}
