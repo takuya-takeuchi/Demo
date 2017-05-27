ed00318d6f7b3ba1af5bcc5a3cdbb8303ed7b5a3
Takuya Takeuchi <spitzbergen@gmail.com>
2017-05-27 16:38:35 +0900

    Initial Commit

---
+++ DotNetCoreLinux4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Must specify full path!!");
+                return;
+            }
+
+            var path = args[0];
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("Specified path does not exist!!");
+                return;
+            }
+
+            FileStream stream = null;
+
+            try
+            {
+                stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.None);
+                Console.WriteLine($"'{path}' can be opened!!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Can not open file. Reason: {ex.Message}!!");
+            }
+            finally
+            {
+                stream?.Dispose();
+            }
+        }
+    }
+}
