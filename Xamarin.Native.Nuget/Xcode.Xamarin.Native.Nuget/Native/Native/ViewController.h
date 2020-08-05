b9c1efe5b25acbbf31327032cd05dd8dc242f10e
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2020-08-05 19:05:31 +0900

    Create Xamarin Native Nuget Package example (#12)
    * include 2 framework
    * check target device (iphone or iphone simulator)
    * build scripts

---
+++ Xamarin.Native.Nuget/Xcode.Xamarin.Native.Nuget/Native/Native/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Native
+//
+//  Created by Takuya Takeuchi on 2020/08/03.
+//  Copyright © 2020 com.takuyatakeuchi.demo.native. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
