0e3a577bcc7e6d4dc536d6c71ad62687de917f75
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2021-04-20 01:42:53 +0900

    Add Xamarin.Native.Nuget and Xamarin.Gtk example (#19)
    * Support Static link for Xamarin.Native.Nuget (#16)
    * Add Xamarin.Gtk example

---
+++ Xamarin.Gtk1/Sample.Gtk/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Prism;
+using Prism.Ioc;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.GTK;
+
+namespace Sample.Gtk
+{
+
+    public class Program
+    {
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            global::Gtk.Application.Init();
+            Forms.Init();
+
+            var app = new App(new GtkInitializer());
+            var window = new FormsWindow();
+            window.LoadApplication(app);
+            window.SetApplicationTitle("Your App Name");
+            window.Show();
+
+            global::Gtk.Application.Run();
+        }
+    }
+
+    public class GtkInitializer : IPlatformInitializer
+    {
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            // Register any platform specific implementations
+        }
+    }
+
+}
---
+++ Xamarin.Gtk1/Sample/App.xaml.cs
@@ -0,0 +1,33 @@
+using Prism;
+using Prism.Ioc;
+using Sample.ViewModels;
+using Sample.Views;
+using Xamarin.Essentials.Implementation;
+using Xamarin.Essentials.Interfaces;
+using Xamarin.Forms;
+
+namespace Sample
+{
+    public partial class App
+    {
+        public App(IPlatformInitializer initializer)
+            : base(initializer)
+        {
+        }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterSingleton<IAppInfo, AppInfoImplementation>();
+
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+        }
+    }
+}
