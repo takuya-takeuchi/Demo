1f7b2aabf8e5680ede07cb6f96c482557e12f201
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2022-01-31 14:55:41 +0900

    feat: add example

---
+++ WinUI/01_Win32API/01_Win32API/IWindowNative.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+// https://github.com/microsoft/microsoft-ui-xaml/issues/2828#issuecomment-653825561
+namespace _01_Win32API
+{
+
+    [ComImport]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    [Guid("EECDBF0E-BAE9-4CB6-A68E-9598E1CB57BB")]
+    public interface IWindowNative
+    {
+
+        IntPtr WindowHandle
+        {
+            get;
+        }
+
+    }
+
+}
---
+++ WinUI/01_Win32API/01_Win32API/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using WinRT;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace _01_Win32API
+{
+
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+
+        #region P/Invokes
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int GetWindowTextLength(IntPtr hwnd);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int GetWindowText(IntPtr hWnd, string lpString, int nMaxCount);
+
+        #endregion
+
+        #region Construtors
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        private async void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Require using WinRT; 
+            var windowNative = this.As<IWindowNative>();
+            var hwnd = windowNative.WindowHandle;
+
+            var length = GetWindowTextLength(hwnd);
+
+            string lpString = new string('0', length + 1);
+            var ret = GetWindowText(hwnd, lpString, length + 1);
+
+            var cd = new ContentDialog
+            {
+                Title = "Hello Win32 API",
+                Content = $"GetWindowText returns '{lpString}' [{ret}]",
+                CloseButtonText = "OK"
+            };
+
+            cd.XamlRoot = this.Content.XamlRoot;
+            await cd.ShowAsync();
+        }
+
+        #endregion
+
+    }
+
+}
