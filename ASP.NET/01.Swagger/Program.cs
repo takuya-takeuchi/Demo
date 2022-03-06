f2c09bfa04c05ff25eb72fd05bb8a2693ec9e347
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2022-03-06 18:25:06 +0900

    Add ASP.NET Core demo using Swagger (#26)

---
+++ ASP.NET/01.Swagger/Program.cs
@@ -0,0 +1,72 @@
+using System.Reflection;
+using Microsoft.OpenApi.Models;
+
+namespace SwaggerApiDemo
+{
+
+    internal sealed class Program
+    {
+
+        #region Methods
+
+        static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "ToDo API",
+                    Description = "An ASP.NET Core Web API for managing ToDo items",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Example Contact",
+                        Url = new Uri("https://example.com/contact")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Example License",
+                        Url = new Uri("https://example.com/license")
+                    }
+                });
+
+                // using System.Reflection;
+                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                    // Show swagger by https://localhost:<port> if set empty
+                    // Otherwise, https://localhost:<port>/swagger shows swagger
+                    options.RoutePrefix = string.Empty;
+                });
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+
+            app.Run();
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ ASP.NET/01.Swagger/README.md
@@ -0,0 +1,45 @@
+# Simple WebAPI (with Swagger)
+
+## Abstacts
+
+* How to create simple web api by using Swagger
+* You can change property name of model by using `JsonPropertyName`
+* You can generate api comments by using xml document
+
+## Requirements
+
+* .NET 6.0 SDK
+
+## How to use?
+
+````powershell
+dotnet run -c Release -- --urls http://localhost:5000
+info: Microsoft.Hosting.Lifetime[14]
+      Now listening on: http://localhost:5000
+info: Microsoft.Hosting.Lifetime[0]
+      Application started. Press Ctrl+C to shut down.
+info: Microsoft.Hosting.Lifetime[0]
+      Hosting environment: Development
+info: Microsoft.Hosting.Lifetime[0]
+      Content root path: D:\Works\OpenSource\Demo\ASP.NET\01.Swagger\
+````
+
+or
+
+````powershell
+dotnet run -c Release
+info: Microsoft.Hosting.Lifetime[14]
+      Now listening on: https://localhost:7262
+info: Microsoft.Hosting.Lifetime[14]
+      Now listening on: http://localhost:5287
+info: Microsoft.Hosting.Lifetime[0]
+      Application started. Press Ctrl+C to shut down.
+info: Microsoft.Hosting.Lifetime[0]
+      Hosting environment: Development
+info: Microsoft.Hosting.Lifetime[0]
+      Content root path: D:\Works\OpenSource\Demo\ASP.NET\01.Swagger\
+````
+
+## Result
+
+[![swagger](./images/preview.png "swagger")](./images/preview.png)
