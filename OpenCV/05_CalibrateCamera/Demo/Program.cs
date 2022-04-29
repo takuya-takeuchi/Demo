5c39596e0afc92ff4e3b200a4d304db140d72062
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2022-04-29 18:15:44 +0900

    fix

--- OpenCV/05_CalibrateCamera/Demo/Program.cs
+++ OpenCV/05_CalibrateCamera/Demo/Program.cs
@@ -57,6 +57,7 @@ namespace Demo
 
         private static void Run(Options option)
         {
+            // setup camera
             using var videoCapture = new VideoCapture();
             videoCapture.Set(VideoCaptureProperties.FrameWidth, 1920);
             videoCapture.Set(VideoCaptureProperties.FrameHeight, 1080);
@@ -64,9 +65,24 @@ namespace Demo
             if (!videoCapture.IsOpened())
                 throw new Exception("VideoCapture initialization failed");
 
-            var patterSize = new Size(option.HorizontalPatternSize, option.VerticalPatternSize);
+            var size = option.Size;
+            var referenceCount = option.Count;
+            var patternSize = new Size(option.HorizontalPatternSize, option.VerticalPatternSize);
 
+            // calibration pattern points in the calibration pattern coordinate space
+            var points = new List<Point3f>();
+            var objectPoints = new List<Mat<Point3f>>();
+            for (var v = 0; v < patternSize.Height; v++)
+            for (var h = 0; h < patternSize.Width; h++)
+                points.Add(new Point3f(h * size, v * size, 0.0f));
+
+            for (var count = 0; count < referenceCount; count++)
+                objectPoints.Add(Mat<Point3f>.FromArray(points));
+
+            // camera buffer frame
             using var frame = new Mat();
+
+            var detectedCorners = new List<Mat<Point2f>>();
 
             var windowName = "Find Chessboard Corners";
             while (true)
@@ -77,11 +93,12 @@ namespace Demo
                     if (frame.Empty())
                         break;
 
-                    // Cv2.ImShow(windowName, frame);
-                    // Cv2.WaitKey(10);
+                    var corners = new Mat<Point2f>();
+                    var result = Cv2.FindChessboardCorners(frame, patternSize, corners);
+                    Cv2.DrawChessboardCorners(frame, patternSize, corners, result);
 
-                    var result = Cv2.FindChessboardCorners(frame, patterSize, out var corners);
-                    Cv2.DrawChessboardCorners(frame, patterSize, corners, result);
+                    // You need not to invoke Cv2.CornerSubPix
+                    // Because cvFindChessboardCorners has already invoke cvFindCornerSubPix
 
                     Cv2.ImShow(windowName, frame);
                     Cv2.WaitKey(10);
@@ -89,15 +106,99 @@ namespace Demo
                     if (!result)
                     {
                         Logger.Error("Failed to detect chessboard corners");
+
+                        corners.Dispose();
                         continue;
+                    }
+
+                    if (result)
+                    {
+                        detectedCorners.Add(corners);
+                        Logger.Info($"Succeeded to detect chessboard corners. [{detectedCorners.Count}/{referenceCount}]");
+                    }
+
+                    if (detectedCorners.Count == referenceCount)
+                    {
+                        Logger.Info("Finish to collect chessboard corners");
+                        break;
                     }
                 }
                 catch (Exception ex)
                 {
+                    Logger.Info($"Failed to detect chessboard corners. Reason: {ex.Message}");
 
+                    Dispose(objectPoints);
+                    Dispose(detectedCorners);
+                    return;
                 }
             }
 
+            Logger.Info("CalibrateCamera...");
+            using var cameraIntrinsicMatrix = new Mat<double>(Mat.Eye(3, 3, MatType.CV_64FC1));
+            using var distortionCoefficients = new Mat<double>();
+            var calibrationFlags = CalibrationFlags.UseIntrinsicGuess | CalibrationFlags.FixK5;
+            var overallRmsReProjectionError = Cv2.CalibrateCamera(objectPoints,
+                                                                  detectedCorners,
+                                                                  frame.Size(),
+                                                                  cameraIntrinsicMatrix,
+                                                                  distortionCoefficients,
+                                                                  out var rotationVectors,
+                                                                  out var translationVectors,
+                                                                  calibrationFlags);
+
+            Logger.Info($"Overall RMS re-projection error: {overallRmsReProjectionError}");
+            Logger.Info($"        Camera Intrinsic Matrix: {cameraIntrinsicMatrix}");
+            Logger.Info($"        Distortion Coefficients: {distortionCoefficients}");
+
+            var dictionary = new Dictionary<string, object>
+            {
+                { "Count", referenceCount },
+                { "ChessSize", size },
+                { "PatternRow", patternSize.Height },
+                { "PatternColumn", patternSize.Width },
+                { "RMS", overallRmsReProjectionError },
+                { "CameraIntrinsicMatrix", cameraIntrinsicMatrix },
+                { "DistortionCoefficients", distortionCoefficients },
+                // { "RotationVectors", rotationVectors },
+                // { "TranslationVectors", translationVectors }
+            };
+            
+            WriteToFile(dictionary, "calibration.yaml");
+        }
+
+        private static void Dispose(IEnumerable<IDisposable> collection)
+        {
+            foreach (var item in collection)
+                item.Dispose();
+        }
+
+        private static void Dispose(IEnumerable<IEnumerable<IDisposable>> collections)
+        {
+            foreach (var collection in collections)
+                Dispose(collection);
+        }
+
+        private static void WriteToFile(Dictionary<string, object> dictionary, string filename)
+        {
+            using var fs = new FileStorage(filename, FileStorage.Modes.Write | FileStorage.Modes.FormatYaml);
+            foreach (var (key, value) in dictionary)
+            {
+                switch (value)
+                {
+                    case int i:
+                        fs.Write(key, i);
+                        break;
+                    case float f:
+                        fs.Write(key, f);
+                        break;
+                    case double d:
+                        fs.Write(key, d);
+                        break;
+                    case Mat mat:
+                        fs.Write(key, mat);
+                        break;
+                }
+            }
         }
 
         #endregion
