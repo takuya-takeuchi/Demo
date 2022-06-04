9c86c1caeaf60e2adfd0c8ec476b715b95368a40
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2022-06-04 21:30:12 +0900

    add template

---
+++ templates/Console/DotNetFramework/Demo/sources/Demo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+
+    internal class Program
+    {
+
+        #region Fields
+        #endregion
+
+        #region Methods
+
+        private static void Main(string[] args)
+        {
+        }
+
+        #region Event Handlers
+        #endregion
+
+        #region Helpers
+        #endregion
+
+        #endregion
+
+    }
+
+}
