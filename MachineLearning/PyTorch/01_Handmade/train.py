f1746d87fc7315e0cf527665c25d1c3d57c79075
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2022-06-05 08:33:12 +0900

    fix

--- MachineLearning/PyTorch/01_Handmade/train.py
+++ MachineLearning/PyTorch/01_Handmade/train.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import datetime
 
 import torch
 import torch.nn as nn
@@ -7,21 +8,26 @@ import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
 
+from tensorboardX import SummaryWriter
 from tqdm import tqdm
 from collections import OrderedDict
 
 from models.net import Net
 import utils
 
+program_name = "01_Handmade"
+
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--epoch", required=True, type=int)
     parser.add_argument("--batchsize", required=True, type=int)
+    parser.add_argument("--tensorboard-logdir", type=str)
     return parser.parse_args()
 
 def train(args):
-    epoch     = args.epoch
-    batchsize = args.batchsize
+    epoch              = args.epoch
+    batchsize          = args.batchsize
+    tensorboard_logdir = args.tensorboard_logdir
 
     # check whether gpu is available
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -53,6 +59,14 @@ def train(args):
                                               total_steps=len(trainloader))
     criterion = nn.CrossEntropyLoss()
 
+    # tensorboard
+    writer = None
+    if os.path.exists(tensorboard_logdir):
+        now = datetime.datetime.now()
+        d = f"{now:%Y%m%d_%H%M%S}"
+        print(f"Setup Tensorboard Summary Writer: {tensorboard_logdir} for {program_name}_{d}")
+        writer = SummaryWriter(log_dir=tensorboard_logdir, comment=f"{program_name}_{d}")
+
     print("Start Training")
 
     for e in range(epoch):
@@ -76,16 +90,18 @@ def train(args):
                 preds = out.argmax(axis=1)
 
                 train_loss += loss.item()
-                accuracy += torch.sum(preds == labels).item() / len(labels)
+                accu = torch.sum(preds == labels).item()
+                accuracy += accu
 
-                pbar.set_postfix(OrderedDict(Loss=loss.item(),
-                                             Accuracy=torch.sum(preds == labels).item() / len(labels)))
-
-            print(f"Epoch: {e + 1}")
-            print(f"Loss: {train_loss / len(trainloader)}")
-            print(f"Accuracy: {accuracy / len(trainloader)}")
+                pbar.set_postfix(OrderedDict(Loss=train_loss / len(trainset),
+                                             Accuracy=accuracy / len(trainset)))
 
             scheduler.step()
+
+        if writer:
+            writer.add_scalar("loss", train_loss / len(trainset), e)
+            writer.add_scalar("accuracy", accuracy / len(trainset), e)     
+            writer.add_scalar("lr", scheduler.get_lr()[0], e)  
     
     print("Finished Training")
 
@@ -94,11 +110,13 @@ def train(args):
 if __name__ == '__main__':
     # parse args
     args = get_args()
-    epoch           = args.epoch
-    batchsize       = args.batchsize
+    epoch              = args.epoch
+    batchsize          = args.batchsize
+    tensorboard_logdir = args.tensorboard_logdir
 
     print("Arguments")
-    print("          epoch: {}".format(epoch))
-    print("      batchsize: {}".format(batchsize))
+    print("               epoch: {}".format(epoch))
+    print("           batchsize: {}".format(batchsize))
+    print("  tensorboard_logdir: {}".format(tensorboard_logdir))
 
     train(args)
