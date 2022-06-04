7f596503136d9d6c71b1282109da21b3efe5a5e9
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2022-06-05 06:28:08 +0900

    fix

--- MachineLearning/PyTorch/00_Tensorboard/run.bat
+++ MachineLearning/PyTorch/00_Tensorboard/run.bat
@@ -1,3 +1,3 @@
 docker build -t tensorboard .
-mkdir $PWD\logs
-docker run -it --detach --rm -p 10000:6006 -v $PWD/logs:/logs tensorboard
+mkdir %CD%\logs
+docker run -it --detach --rm -p 10000:6006 -v %CD%\logs:/logs tensorboard
