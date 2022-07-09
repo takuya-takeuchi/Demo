58a7edd314b243d2232eed4a8dd689e396582e73
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2022-07-10 05:00:51 +0900

    fix

---
+++ TestFramework/Appium/Appium2/TestApp/ViewModels/Interfaces/IMainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace TestApp.ViewModels.Interfaces
+{
+
+    public interface IMainWindowViewModel
+    {
+
+        string Title
+        {
+            get;
+        }
+
+        ICommand GeneratePrimeNumberCommand
+        {
+            get;
+        }
+
+        ICommand GenerateNonPrimeNumberCommand
+        {
+            get;
+        }
+
+        ObservableCollection<uint> Numbers
+        {
+            get;
+        }
+
+    }
+
+}
