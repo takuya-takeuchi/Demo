22b60e7f8541684fba18232f54fcde668bfafbee
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2022-09-23 16:28:33 +0900

    Add examples of Docker Remote API (#80)

---
+++ Docker/01_TestRemoteAPI/README.md
@@ -0,0 +1,50 @@
+# Test Docker Remote API
+
+## Abstacts
+
+* How to control docker container via Docker Remote API
+
+## Requirements
+
+* .NET 6.0 SDK
+* PowerShell Core 6.2 or later
+* docker
+
+## Dependencies
+
+* [Docker.DotNet](https://github.com/dotnet/Docker.DotNet)
+  * MIT License
+* [NLog](https://github.com/NLog/NLog)
+  * BSD-3-Clause License
+
+## How to use
+
+### Enable Docker Remote API
+
+#### Windows 
+
+<img src="./images/docker-desktop.png" />
+
+#### Linux using systemd
+
+Edit `/lib/systemd/system/docker.service`
+
+````diff
+- ExecStart=/usr/bin/dockerd -H fd:// --containerd=/run/containerd/containerd.sock
++ ExecStart=/usr/bin/dockerd -H fd:// -H tcp://0.0.0.0:2375 --containerd=/run/containerd/containerd.sock
+````
+
+Then,
+
+````shell
+$ sudo systemctl daemon-reload
+$ sudo systemctl restart docker.service
+````
+
+### Run demo program
+
+````shell
+$ dotnet run -c Release --project sources\Demo\Demo.csproj -- tcp://localhost:2375
+2022-08-21 10:09:25.3153 [INFO ] CONTAINER ID: sha256:d6ef2c07430b8acc76603104e07cf1b906f09f630b13211fba6ea4117ddda791, Name: /docker-sample-nginx
+2022-08-21 10:09:25.3536 [INFO ] CONTAINER ID: sha256:7cd80bdefc6bc7ba661f1a208ba172b9f0e7ce7adfe665c4539ca81a9c82d311, Name: /docker-sample-aspnet6
+````
---
+++ Docker/01_TestRemoteAPI/sources/Demo/Program.cs
@@ -0,0 +1,50 @@
+﻿using Docker.DotNet;
+using Docker.DotNet.Models;
+using NLog;
+
+namespace Demo
+{
+
+    internal sealed class Program
+    {
+
+        #region Fields
+
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        #endregion
+
+        #region Methods
+
+        private static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Logger.Error($"{nameof(Demo)} <docker remote api endpoint>");
+                return;
+            }
+
+            var client = new DockerClientConfiguration(new Uri(args[0])).CreateClient();
+            var containers = client.Containers.ListContainersAsync(new ContainersListParameters
+            {
+                All = true
+            }).Result?.ToArray() ?? Array.Empty<ContainerListResponse>();
+
+            if (!containers.Any())
+            {
+                Logger.Info("There is not active containers");
+                return;
+            }
+
+            foreach (var container in containers)
+            {
+                var names = string.Join(',', container.Names);
+                Logger.Info($"CONTAINER ID: {container.ImageID}, Name: {names}");
+            }
+        }
+
+        #endregion
+
+    }
+
+}
