fb912679d58f53cdb7e82a1fc09c9499020d8b77
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2022-10-08 21:23:54 +0900

    fix

---
+++ templates/Desktop/Windows Presentation Foundation/DotNet/Prism/Demo/sources/Demo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+
+using Demo.ViewModels.Interfaces;
+using System.Windows.Controls;
+using Demo.Views.Modules;
+
+namespace Demo.ViewModels
+{
+
+    internal sealed class MainWindowViewModel : BindableBase, IMainWindowViewModel
+    {
+
+        #region Fields
+
+        public readonly IRegionManager _RegionManager;
+
+        #endregion
+
+        #region Constructors
+
+        public MainWindowViewModel(IRegionManager regionManager)
+        {
+            this._RegionManager = regionManager;
+
+            this.LoadedCommand = new DelegateCommand(() =>
+            {
+                this._RegionManager.RequestNavigate(this.RegionName, nameof(MainModule));
+            });
+        }
+
+        #endregion
+
+        #region IMainWindowViewModel Members
+
+        public DelegateCommand LoadedCommand
+        {
+            get;
+        }
+
+        public string RegionName => nameof(MainWindowViewModel);
+
+        #endregion
+
+    }
+
+}
