c30589384f78ef4617dc0d6b1776b3efdf67c994
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2022-10-22 15:55:50 +0900

    fix

---
+++ Misc/11_CSharpEnforceCodeStyleInBuild/sources/Demo/Program.cs
@@ -0,0 +1,27 @@
+﻿using NLog;
+
+namespace Demo
+{
+
+    internal sealed class Program
+    {
+
+        #region Fields
+
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        #endregion
+
+        #region Methods
+
+        private static void Main()
+        {
+            string message = "Start";
+            Logger.Info(message);
+        }
+
+        #endregion
+
+    }
+
+}
