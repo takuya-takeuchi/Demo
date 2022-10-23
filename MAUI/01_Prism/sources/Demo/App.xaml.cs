89e603c0f3ea89fe2133bad31f608e394cdd9702
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2022-10-23 09:46:08 +0900

    fix

---
+++ MAUI/01_Prism/sources/Demo/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace Demo
+{
+
+    public partial class App : Application
+    {
+
+        #region Constructors
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+        #endregion
+
+    }
+
+}
