9a01e9753346b29937a8fe9ad33fb43e8b40e35f
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2022-11-20 13:13:06 +0900

    Add example of conversion from pytorch to onnx (#100)

---
+++ MachineLearning/PyTorch/05_ConvertToOnnx/convert.py
@@ -0,0 +1,64 @@
+import argparse
+import math
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+from PIL import Image
+
+from models.lenet import LeNet
+import utils
+
+# setup logger
+logger = utils.get_logger("convert")
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--pretrained", required=True, type=str)
+    return parser.parse_args()
+
+def train(args):
+    pretrained = args.pretrained
+
+    # check whether gpu is available
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    # setup network
+    model = LeNet()
+    model.load_state_dict(torch.load(pretrained))
+    model.to(device)
+    model.eval()
+
+    print(model)
+    # LeNet(
+    #     (conv1): Conv2d(1, 6, kernel_size=(5, 5), stride=(1, 1), padding=(2, 2))
+    #     (pool1): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+    #     (conv2): Conv2d(6, 16, kernel_size=(5, 5), stride=(1, 1))
+    #     (pool2): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False)
+    #     (flatten): Flatten(start_dim=1, end_dim=-1)
+    #     (fc1): Linear(in_features=400, out_features=120, bias=True)
+    #     (fc2): Linear(in_features=120, out_features=84, bias=True)
+    #     (fc3): Linear(in_features=84, out_features=10, bias=True)
+    # )
+
+    logger.info("Start Conversion")
+
+    batch = 1
+    channel = 1
+    height = 28
+    width = 28
+    dummy = torch.randn((batch, channel, height, width))
+    torch.onnx.export(model, dummy.to(device), "lenet.onnx", verbose=True)
+
+    logger.info("Finished Conversion")
+
+if __name__ == '__main__':
+    # parse args
+    args = get_args()
+    pretrained = args.pretrained
+
+    logger.info("Arguments")
+    logger.info("     pretrained: {}".format(pretrained))
+
+    train(args)
---
+++ MachineLearning/PyTorch/05_ConvertToOnnx/train.py
@@ -0,0 +1,125 @@
+import os
+import argparse
+import datetime
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+
+from tensorboardX import SummaryWriter
+from tqdm import tqdm
+from collections import OrderedDict
+
+from models.lenet import LeNet
+import utils
+
+program_name = "02_LeNet"
+
+# setup logger
+logger = utils.get_logger("train")
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--epoch", required=True, type=int)
+    parser.add_argument("--batchsize", required=True, type=int)
+    parser.add_argument("--tensorboard-logdir", type=str)
+    return parser.parse_args()
+
+def train(args):
+    epoch              = args.epoch
+    batchsize          = args.batchsize
+    tensorboard_logdir = args.tensorboard_logdir
+
+    # check whether gpu is available
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    # setup transform
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5), (0.5))
+    ])
+
+    # setup data loader for train
+    trainset = torchvision.datasets.MNIST(root='../data',
+                                          train=True,
+                                          download=True,
+                                          transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset,
+                                              batch_size=batchsize,
+                                              shuffle=True)
+
+    # setup network, optimizer, scheduler and criterion
+    model = LeNet()
+    model.to(device)
+    model.train()
+    optimizer = optim.Adam(params=model.parameters(),
+                          lr=1e-3)
+    scheduler = optim.lr_scheduler.OneCycleLR(optimizer=optimizer,
+                                              max_lr=1e-3,
+                                              total_steps=len(trainloader))
+    criterion = nn.CrossEntropyLoss()
+
+    # tensorboard
+    writer = None
+    if os.path.exists(tensorboard_logdir):
+        now = datetime.datetime.now()
+        d = f"{now:%Y%m%d_%H%M%S}"
+        logger.info(f"Setup Tensorboard Summary Writer: {tensorboard_logdir} for {program_name}_{d}")
+        writer = SummaryWriter(log_dir=tensorboard_logdir, comment=f"{program_name}_{d}")
+
+    logger.info("Start Training")
+
+    for e in range(epoch):
+        accuracy = 0.0
+        train_loss = 0.0
+
+        with tqdm(trainloader) as pbar:
+            pbar.set_description(f"[Epoch {e + 1}/{epoch}]")
+
+            for images, labels in pbar:
+                optimizer.zero_grad()
+                
+                images = images.to(device)
+                labels = labels.to(device)
+
+                out = model(images)
+                loss = criterion(out, labels)
+
+                loss.backward()
+                optimizer.step()
+
+                preds = out.argmax(axis=1)
+
+                train_loss += loss.item()
+                accu = torch.sum(preds == labels).item()
+                accuracy += accu
+
+                pbar.set_postfix(OrderedDict(Loss=train_loss / len(trainset),
+                                             Accuracy=accuracy / len(trainset)))
+
+            scheduler.step()
+
+        if writer:
+            writer.add_scalar("loss", train_loss / len(trainset), e)
+            writer.add_scalar("accuracy", accuracy / len(trainset), e)     
+            writer.add_scalar("lr", scheduler.get_last_lr()[0], e)  
+    
+    logger.info("Finished Training")
+
+    torch.save(model.state_dict(), "trained.pth")
+
+if __name__ == '__main__':
+    # parse args
+    args = get_args()
+    epoch              = args.epoch
+    batchsize          = args.batchsize
+    tensorboard_logdir = args.tensorboard_logdir
+
+    logger.info("Arguments")
+    logger.info("               epoch: {}".format(epoch))
+    logger.info("           batchsize: {}".format(batchsize))
+    logger.info("  tensorboard_logdir: {}".format(tensorboard_logdir))
+
+    train(args)
