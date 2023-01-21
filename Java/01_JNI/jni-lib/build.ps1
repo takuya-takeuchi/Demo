fcae48e74a876786c90fba3cc51a5e7f07ea08ef
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2023-01-21 12:48:35 +0900

    fix

--- Java/01_JNI/jni-lib/build.ps1
+++ Java/01_JNI/jni-lib/build.ps1
@@ -4,6 +4,15 @@ $buildDir = Join-Path $current build | `
 New-Item -Type Directory $buildDir -Force | Out-Null
 
 Push-Location $buildDir
-cmake ${current}
+
+if ($global:IsWindows)
+{
+    cmake -G "Visual Studio 17 2022" -A x64 -T host=x64 ${current}
+}
+else
+{
+    cmake ${current}
+}
+
 cmake --build . --config Release
 Pop-Location
