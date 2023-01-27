a8396f6c43290f8ef4ba57560137f6c32337ff10
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2023-01-28 02:10:01 +0900

    fix

---
+++ CMake/08_curl/main.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <curl/curl.h>
+
+int main()
+{
+    CURLcode ret;
+
+    CURL* hnd = curl_easy_init();
+    curl_easy_setopt(hnd, CURLOPT_URL, "www.google.com");
+    curl_easy_setopt(hnd, CURLOPT_NOPROGRESS, 1L);
+    curl_easy_setopt(hnd, CURLOPT_USERAGENT, "curl/7.87.0");
+    curl_easy_setopt(hnd, CURLOPT_MAXREDIRS, 50L);
+    curl_easy_setopt(hnd, CURLOPT_HTTP_VERSION, (long)CURL_HTTP_VERSION_2TLS);
+    curl_easy_setopt(hnd, CURLOPT_TCP_KEEPALIVE, 1L);
+
+    ret = curl_easy_perform(hnd);
+
+    curl_easy_cleanup(hnd);
+    hnd = nullptr;
+
+    return (int)ret;
+}
