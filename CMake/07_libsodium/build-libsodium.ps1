5037ab5616937fa4cb4269b0d03d504e1db51fd1
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2023-01-28 00:22:50 +0900

    Add minimal example of libsodium from cmake (#120)

---
+++ CMake/07_libsodium/build-libsodium.ps1
@@ -0,0 +1,91 @@
+#***************************************
+#Arguments
+#%1: Build Configuration (Release/Debug)
+#***************************************
+Param
+(
+   [Parameter(
+   Mandatory=$True,
+   Position = 1
+   )][string]
+   $Configuration
+)
+
+$current = $PSScriptRoot
+
+# update submodule
+git submodule update --init --recursive .
+
+# get os name
+if ($global:IsWindows)
+{
+    $os = "win"
+}
+elseif ($global:IsMacOS)
+{
+    $os = "osx"
+}
+elseif ($global:IsLinux)
+{
+    $os = "linux"
+}
+
+$target = "libsodium"
+
+$sourceDir = Join-Path $current $target
+$installDir = Join-Path $current install | `
+              Join-Path -ChildPath $os | `
+              Join-Path -ChildPath $target
+
+New-Item -Type Directory $installDir -Force | Out-Null
+
+Push-Location $sourceDir
+if ($global:IsWindows)
+{
+    $build_dir = Join-Path ${sourceDir} builds | `
+                 Join-Path -ChildPath msvc | `
+                 Join-Path -ChildPath vs2019
+    Push-Location $build_dir
+
+    $MSBuild = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe"
+    if (!(Test-Path ${MSBuild}))
+    {
+       $MSBuild = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe"
+       if (!(Test-Path ${MSBuild}))
+       {
+          $MSBuild = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"
+       }
+    }
+    
+    # & ${MSBuild} "libsodium.sln" "/m" "/t:clean;rebuild" "/p:Configuration=Dyn${Configuration};Platform=${arch}"
+    & ${MSBuild} "libsodium.sln" "/m" "/t:clean;rebuild" "/p:Configuration=Static${Configuration};Platform=x64"    
+
+    $build_dir = Join-Path ${sourceDir} bin | `
+                 Join-Path -ChildPath x64 | `
+                 Join-Path -ChildPath ${Configuration} | `
+                 Join-Path -ChildPath v142
+
+    # lib
+    $dst = Join-Path $installDir lib
+    New-Item $dst -Force -ItemType Directory | Out-Null
+    Copy-Item ${build_dir}/* $dst -Force -Recurse
+    # include    
+    $dst = Join-Path $installDir include
+    $include_dir = Join-Path ${sourceDir} src | `
+                   Join-Path -ChildPath libsodium | `
+                   Join-Path -ChildPath include
+    Copy-Item ${include_dir}/*.h $dst -Recurse -Force
+    Copy-Item ${include_dir}/sodium $dst -Recurse -Force
+
+    Pop-Location
+}
+else
+{
+    chmod +x *.sh
+    make clean 
+    ./autogen.sh
+    ./configure --disable-dependency-tracking --prefix=$installDir --with-pic="yes"
+    make -j8
+    make install
+}
+Pop-Location
---
+++ CMake/07_libsodium/main.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+// override SODIUM_EXPORT if use libsodium as static
+#define SODIUM_DLL_EXPORT
+#include <sodium.h>
+
+int main()
+{    
+    const int32_t ret = sodium_init();
+    if (ret < 0)
+    {
+        std::cout << "[Error] libsodium couldn't be initialized with sodium_init()" << std::endl;
+    }
+    else
+    {
+        std::cout << "[Info] sodium_init: " << ret << std::endl;
+    }
+}
