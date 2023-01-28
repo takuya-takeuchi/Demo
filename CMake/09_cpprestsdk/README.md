e34dd3e43591cbd92f02fac18cf037e93fb03d11
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2023-01-29 01:18:10 +0900

    fix

--- CMake/09_cpprestsdk/README.md
+++ CMake/09_cpprestsdk/README.md
@@ -55,4 +55,14 @@ You have to set `VCPKG_ROOT_DIR` envrironmental variable, like `C:\vcpkg` before
 $ pwsh build-boost.ps1 <Debug/Release>
 $ pwsh build-cpprestsdk.ps1  <Debug/Release>
 $ pwsh build.ps1  <Debug/Release>
+{
+  "args": {},
+  "headers": {
+    "Host": "httpbin.org",
+    "User-Agent": "cpprestsdk/2.10.18",
+    "X-Amzn-Trace-Id": "Root=1-63d54a6e-3724cf68667705a472c789c0"
+  },
+  "origin": "14.11.38.64", 
+  "url": "https://httpbin.org/get"
+}
 ````
--- CMake/09_cpprestsdk/main.cpp
+++ CMake/09_cpprestsdk/main.cpp
@@ -1,30 +1,24 @@
 ﻿#include <cpprest/http_client.h>
 #include <cpprest/filestream.h>
 
-using namespace utility;                    // 文字列変換などの一般的なユーティリティ
-using namespace web;                        // URIのような共通の機能
-using namespace web::http;                  // 共通のHTTP機能
-using namespace web::http::client;          // HTTP クライアントの機能
-using namespace concurrency::streams;       // 非同期ストリーム
+using namespace utility;
+using namespace web;
+using namespace web::http;
+using namespace web::http::client;
+using namespace concurrency::streams;
 
 pplx::task<void> GetTest()
 {
     return pplx::create_task([] 
     {
-        // クライアントの設定
-        http_client client(L"https://httpbin.org/get");
-
-        // リクエスト送信
+        http_client client(::utility::conversions::to_string_t("https://httpbin.org/get"));
         return client.request(methods::GET);
-
     })
         
     .then([](http_response  response)
     {
-        // ステータスコード判定
         if (response.status_code() == status_codes::OK)
         {
-            // レスポンスボディを表示
             auto body = response.extract_string();
             std::wcout << body.get().c_str() << std::endl;
         }
@@ -39,7 +33,8 @@ int main(void)
     }
     catch (const std::exception& e)
     {
-        printf("Error exception:%s\n", e.what());
+        printf("Error exception: %s\n", e.what());
     }
+
     return 0;
 }
