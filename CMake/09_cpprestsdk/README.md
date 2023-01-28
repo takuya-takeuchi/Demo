72dba38d763a34a4d055b2f642222d092a63fc21
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2023-01-29 01:44:47 +0900

    Add minimal example of cpprestsdk from cmake (#122)

---
+++ CMake/09_cpprestsdk/README.md
@@ -0,0 +1,68 @@
+# cpprestsdk from CMake
+
+## Abstracts
+
+* Build and link cpprestsdk by cmake
+  * static build
+* Minimal example program
+
+## Requirements
+
+### Common
+
+* Powershell
+* CMake 3.0.0 or later
+* C++ Compiler supports C++11
+
+### Windows
+
+* Visual Studio
+* OpenSSL
+  * via `vcpkg` command with `--triplet x64-windows-static`
+* zlib
+  * via `vcpkg` command with `--triplet x64-windows-static`
+
+### Ubuntu
+
+* g++
+* libz-dev
+  * via `apt` command
+* libssl-dev
+  * via `apt` command
+
+### OSX
+
+* Xcode
+* websocketpp
+  * via `brew` command
+    * cpprestsdk can not build due to https://github.com/zaphoyd/websocketpp/issues/1004
+
+## Dependencies
+
+* [cpprestsdk](https://github.com/microsoft/cpprestsdk)
+  * 2.10.18
+    * use e1b6a8e61d6b3ab98734b0532bad1da46458212a for only osx because [Fix likely typo in SafeInt3.hpp, that results in error with clang 15](https://github.com/microsoft/cpprestsdk/pull/1711)
+  * MIT License
+* [Boost](https://www.boost.org/)
+  * v1.81.0
+  * Boost License
+
+## How to usage?
+
+You have to set `VCPKG_ROOT_DIR` envrironmental variable, like `C:\vcpkg` before build cpprestsdk on windows machine.
+
+````shell
+$ pwsh build-boost.ps1 <Debug/Release>
+$ pwsh build-cpprestsdk.ps1  <Debug/Release>
+$ pwsh build.ps1  <Debug/Release>
+{
+  "args": {},
+  "headers": {
+    "Host": "httpbin.org",
+    "User-Agent": "cpprestsdk/2.10.18",
+    "X-Amzn-Trace-Id": "Root=1-63d54a6e-3724cf68667705a472c789c0"
+  },
+  "origin": "14.11.38.64", 
+  "url": "https://httpbin.org/get"
+}
+````
---
+++ CMake/09_cpprestsdk/build-cpprestsdk.ps1
@@ -0,0 +1,107 @@
+#***************************************
+#Arguments
+#%1: Build Configuration (Release/Debug)
+#***************************************
+Param
+(
+   [Parameter(
+   Mandatory=$True,
+   Position = 1
+   )][string]
+   $Configuration
+)
+
+$current = $PSScriptRoot
+
+# update submodule
+git submodule update --init --recursive .
+
+# get os name
+if ($global:IsWindows)
+{
+    $os = "win"
+}
+elseif ($global:IsMacOS)
+{
+    $os = "osx"
+    Push-Location
+    Pop-Location
+}
+elseif ($global:IsLinux)
+{
+    $os = "linux"
+}
+
+$target = "cpprestsdk"
+
+$sourceDir = Join-Path $current $target
+$buildDir = Join-Path $current build | `
+            Join-Path -ChildPath $os | `
+            Join-Path -ChildPath $target
+$installDir = Join-Path $current install | `
+              Join-Path -ChildPath $os | `
+              Join-Path -ChildPath $target
+
+$boostDir = Join-Path $current install | `
+            Join-Path -ChildPath $os | `
+            Join-Path -ChildPath boost | `
+            Join-Path -ChildPath 1.81.0 | `
+            Join-Path -ChildPath lib | `
+            Join-Path -ChildPath cmake
+
+New-Item -Type Directory $buildDir -Force | Out-Null
+New-Item -Type Directory $installDir -Force | Out-Null
+
+# path Fix likely typo in SafeInt3.hpp, that results in error with clang 15 (https://github.com/microsoft/cpprestsdk/pull/1711)
+if ($global:IsMacOS)
+{
+    Push-Location $sourceDir
+    git checkout e1b6a8e61d6b3ab98734b0532bad1da46458212a
+    Pop-Location
+}
+
+Push-Location $buildDir
+if ($global:IsWindows)
+{
+    if (!($env:VCPKG_ROOT_DIR))
+    {
+        Write-Host "VCPKG_ROOT_DIR environmental variable is missing" -ForegroundColor Red
+        return
+    }
+
+    $toolchain = "${env:VCPKG_ROOT_DIR}\scripts\buildsystems\vcpkg.cmake"
+    if (!(Test-Path(${toolchain})))
+    {
+        Write-Host "${toolchain} is missing" -ForegroundColor Red
+        return
+    }
+
+    cmake -D CMAKE_INSTALL_PREFIX=$installDir `
+          -D CMAKE_BUILD_TYPE=$Configuration `
+          -D CMAKE_PREFIX_PATH=${boostDir} `
+          -D BUILD_SHARED_LIBS=OFF `
+          -D Boost_USE_STATIC_LIBS=ON `
+          -D CMAKE_TOOLCHAIN_FILE="${env:VCPKG_ROOT_DIR}\scripts\buildsystems\vcpkg.cmake" `
+          -D VCPKG_TARGET_TRIPLET="x64-windows-static" `
+          $sourceDir
+}
+elseif ($global:IsMacOS)
+{
+    cmake -D CMAKE_INSTALL_PREFIX=$installDir `
+          -D CMAKE_BUILD_TYPE=$Configuration `
+          -D CMAKE_PREFIX_PATH=${boostDir} `
+          -D BUILD_SHARED_LIBS=OFF `
+          -D Boost_USE_STATIC_LIBS=ON `
+          $sourceDir
+}
+elseif ($global:IsLinux)
+{
+    cmake -D CMAKE_INSTALL_PREFIX=$installDir `
+          -D CMAKE_BUILD_TYPE=$Configuration `
+          -D CMAKE_PREFIX_PATH=${boostDir} `
+          -D BUILD_SHARED_LIBS=OFF `
+          -D Boost_USE_STATIC_LIBS=ON `
+          $sourceDir
+}
+cmake --build . --config $Configuration --target install
+Pop-Location
