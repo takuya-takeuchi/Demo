3a3a791645f97b2675e76517341687820db72f7e
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2023-01-28 20:40:54 +0900

    fix

---
+++ CMake/09_cpprestsdk/README.md
@@ -0,0 +1,44 @@
+# cpprestsdk from CMake
+
+## Abstracts
+
+* Build and link cpprestsdk by cmake
+  * static build
+* Minimal example program
+
+## Requirements
+
+### Common
+
+* Powershell
+* CMake 3.0.0 or later
+* C++ Compiler supports C++11
+
+### Windows
+
+* Visual Studio
+
+### Ubuntu
+
+* g++
+
+### OSX
+
+* Xcode
+
+## Dependencies
+
+* [cpprestsdk](https://github.com/microsoft/cpprestsdk)
+  * 2.10.18
+  * MIT License
+* [Boost](https://www.boost.org/)
+  * v1.81.0
+  * Boost License
+
+## How to usage?
+
+````shell
+$ pwsh build-boost.ps1 <Debug/Release>
+$ pwsh build-cpprestsdk.ps1  <Debug/Release>
+$ pwsh build.ps1  <Debug/Release>
+````
---
+++ CMake/09_cpprestsdk/build-cpprestsdk.ps1
@@ -0,0 +1,82 @@
+#***************************************
+#Arguments
+#%1: Build Configuration (Release/Debug)
+#***************************************
+Param
+(
+   [Parameter(
+   Mandatory=$True,
+   Position = 1
+   )][string]
+   $Configuration
+)
+
+$current = $PSScriptRoot
+
+# update submodule
+git submodule update --init --recursive .
+
+# get os name
+if ($global:IsWindows)
+{
+    $os = "win"
+}
+elseif ($global:IsMacOS)
+{
+    $os = "osx"
+}
+elseif ($global:IsLinux)
+{
+    $os = "linux"
+}
+
+$target = "cpprestsdk"
+
+$sourceDir = Join-Path $current $target
+$buildDir = Join-Path $current build | `
+            Join-Path -ChildPath $os | `
+            Join-Path -ChildPath $target
+$installDir = Join-Path $current install | `
+              Join-Path -ChildPath $os | `
+              Join-Path -ChildPath $target
+
+$boostDir = Join-Path $current install | `
+            Join-Path -ChildPath $os | `
+            Join-Path -ChildPath boost | `
+            Join-Path -ChildPath 1.81.0 | `
+            Join-Path -ChildPath lib | `
+            Join-Path -ChildPath cmake
+
+New-Item -Type Directory $buildDir -Force | Out-Null
+New-Item -Type Directory $installDir -Force | Out-Null
+
+Push-Location $buildDir
+if ($global:IsWindows)
+{
+    cmake -D CMAKE_INSTALL_PREFIX=$installDir `
+          -D CMAKE_BUILD_TYPE=$Configuration `
+          -D CMAKE_PREFIX_PATH=${boostDir} `
+          -D BUILD_SHARED_LIBS=OFF `
+          -D Boost_USE_STATIC_LIBS=ON `
+          $sourceDir
+}
+elseif ($global:IsMacOS)
+{
+    cmake -D CMAKE_INSTALL_PREFIX=$installDir `
+          -D CMAKE_BUILD_TYPE=$Configuration `
+          -D CMAKE_PREFIX_PATH=${boostDir} `
+          -D BUILD_SHARED_LIBS=OFF `
+          -D Boost_USE_STATIC_LIBS=ON `
+          $sourceDir
+}
+elseif ($global:IsLinux)
+{
+    cmake -D CMAKE_INSTALL_PREFIX=$installDir `
+          -D CMAKE_BUILD_TYPE=$Configuration `
+          -D CMAKE_PREFIX_PATH=${boostDir} `
+          -D BUILD_SHARED_LIBS=OFF `
+          -D Boost_USE_STATIC_LIBS=ON `
+          $sourceDir
+}
+cmake --build . --config $Configuration --target install
+Pop-Location
