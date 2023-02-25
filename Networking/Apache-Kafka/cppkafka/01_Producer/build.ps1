7f7b52319e9bf2e680f6004a37269fa215ee8e90
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2023-02-26 03:54:32 +0900

    fix

---
+++ Networking/Apache-Kafka/cppkafka/01_Producer/build.ps1
@@ -0,0 +1,126 @@
+#***************************************
+#Arguments
+#%1: Build Configuration (Release/Debug)
+#***************************************
+Param
+(
+   [Parameter(
+   Mandatory=$True,
+   Position = 1
+   )][string]
+   $Configuration
+)
+
+$current = $PSScriptRoot
+
+# get os name
+if ($global:IsWindows)
+{
+    $os = "win"
+}
+elseif ($global:IsMacOS)
+{
+    $os = "osx"
+}
+elseif ($global:IsLinux)
+{
+    $os = "linux"
+}
+
+$target = "cppkafka"
+
+# build
+$sourceDir = $current
+$buildDir = Join-Path $current build | `
+            Join-Path -ChildPath $os | `
+            Join-Path -ChildPath program
+$installDir = Join-Path $current install | `
+              Join-Path -ChildPath $os
+
+$targetDir = Join-Path $installDir $target | `
+             Join-Path -ChildPath lib | `
+             Join-Path -ChildPath cmake
+
+$rootDir = Split-Path $current -Parent
+$cppkafkaInstallDir = Join-Path $rootDir install | `
+                      Join-Path -ChildPath $os | `
+                      Join-Path -ChildPath $target
+
+New-Item -Type Directory $buildDir -Force | Out-Null
+New-Item -Type Directory $installDir -Force | Out-Null
+
+Push-Location $buildDir
+if ($global:IsWindows)
+{
+    if (!($env:VCPKG_ROOT_DIR))
+    {
+        Write-Host "VCPKG_ROOT_DIR environmental variable is missing" -ForegroundColor Red
+        return
+    }
+
+    $toolchain = "${env:VCPKG_ROOT_DIR}\scripts\buildsystems\vcpkg.cmake"
+    if (!(Test-Path(${toolchain})))
+    {
+        Write-Host "${toolchain} is missing" -ForegroundColor Red
+        return
+    }
+
+    $cpp_kafka_include_dir = Join-Path ${cppkafkaInstallDir} include
+    $cpp_kafka_libraries = Join-Path ${cppkafkaInstallDir} lib | `
+                           Join-Path -ChildPath cppkafka.lib
+    cmake -D CMAKE_INSTALL_PREFIX=${installDir} `
+          -D CMAKE_PREFIX_PATH="${targetDir}" `
+          -D CMAKE_TOOLCHAIN_FILE="${env:VCPKG_ROOT_DIR}\scripts\buildsystems\vcpkg.cmake" `
+          -D VCPKG_TARGET_TRIPLET="x64-windows-static" `
+          -D CPPKAFKA_INCLUDE_DIR="${cpp_kafka_include_dir}" `
+          -D CPPKAFKA_LIBRARIES="${cpp_kafka_libraries}" `
+          $sourceDir
+}
+elseif ($global:IsMacOS)
+{
+    $cpp_kafka_include_dir = Join-Path ${cppkafkaInstallDir} include
+    $cpp_kafka_libraries = Join-Path ${cppkafkaInstallDir} lib | `
+                           Join-Path -ChildPath cppkafka.lib
+    cmake -D CMAKE_INSTALL_PREFIX=${installDir} `
+          -D CMAKE_PREFIX_PATH="${targetDir}" `
+          -D CPPKAFKA_INCLUDE_DIR="${cpp_kafka_include_dir}" `
+          -D CPPKAFKA_LIBRARIES="${cpp_kafka_libraries}" `
+          $sourceDir
+}
+elseif ($global:IsLinux)
+{
+    $cpp_kafka_include_dir = Join-Path ${cppkafkaInstallDir} include
+    $cpp_kafka_libraries = Join-Path ${cppkafkaInstallDir} lib | `
+                           Join-Path -ChildPath cppkafka.lib
+    cmake -D CMAKE_INSTALL_PREFIX=${installDir} `
+          -D CMAKE_PREFIX_PATH="${targetDir}" `
+          -D CPPKAFKA_INCLUDE_DIR="${cpp_kafka_include_dir}" `
+          -D CPPKAFKA_LIBRARIES="${cpp_kafka_libraries}" `
+          $sourceDir
+}
+cmake --build . --config ${Configuration} --target install
+Pop-Location
+
+# run
+if ($global:IsWindows)
+{
+    $programDir = Join-Path $current build | `
+                  Join-Path -ChildPath $os | `
+                  Join-Path -ChildPath program | `
+                  Join-Path -ChildPath ${Configuration}
+    $program = Join-Path $programDir Test.exe
+}
+elseif ($global:IsMacOS)
+{
+    $programDir = Join-Path $current build | `
+                  Join-Path -ChildPath $os | `
+                  Join-Path -ChildPath program
+    $program = Join-Path $programDir Test
+}
+elseif ($global:IsLinux)
+{
+    $programDir = Join-Path $current build | `
+                  Join-Path -ChildPath $os | `
+                  Join-Path -ChildPath program
+    $program = Join-Path $programDir Test
+}
---
+++ Networking/Apache-Kafka/cppkafka/01_Producer/main.cpp
@@ -0,0 +1,33 @@
+﻿#include <cstdint>
+#include <iostream>
+#include <fstream>
+
+#include <cppkafka/cppkafka.h>
+
+using namespace cppkafka;
+
+int32_t main(int32_t argc, const char** argv)
+{
+    if (argc != 4)
+    {
+        std::cout << "Usage: Demo <kafka server> <topic name> <message>" << std::endl;
+        return -1;
+    }
+
+    const auto server = argv[1];
+    const auto topic = argv[2];
+    const auto message = argv[3];
+    
+    // Create the config
+    Configuration config =
+    {
+        { "metadata.broker.list", std::string(server) }
+    };
+
+    Producer producer(config);
+
+    producer.produce(MessageBuilder(std::string(topic)).partition(0).payload(std::string(message)));
+    producer.flush();
+
+    return 0;
+}
