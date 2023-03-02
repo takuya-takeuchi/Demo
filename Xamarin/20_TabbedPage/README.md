6272def6954b4f8305562221171ddc367894d9e5
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2023-03-02 21:39:27 +0900

    init

---
+++ Xamarin/20_TabbedPage/README.md
@@ -0,0 +1,28 @@
+# template
+
+## Abstracts
+
+* todo
+
+## Requirements
+
+* Visual Studio 2022
+* Xamarin
+
+## Dependencies
+
+* [Font Awesome Free](https://fontawesome.com/)
+  * Fonts
+    * SIL Open Font License
+* [NLog](https://github.com/NLog/NLog)
+  * BSD-3-Clause License
+* [Prism.Unity.Forms](https://github.com/PrismLibrary/Prism)
+  * MIT License
+* [Xamarin.Essentials.Interfaces](https://github.com/rdavisau/essential-interfaces)
+  * MIT License
+* [Xamarin.Forms](https://github.com/xamarin/Xamarin.Forms)
+  * MIT License
+
+## How to usage?
+
+todo
---
+++ Xamarin/20_TabbedPage/sources/Demo/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,50 @@
+﻿using Prism.Commands;
+using Prism.Navigation;
+
+using Demo.Services.Interfaces;
+using Demo.ViewModels.Interfaces;
+
+namespace Demo.ViewModels
+{
+
+    public sealed class MainPageViewModel : ViewModelBase, IMainPageViewModel
+    {
+
+        #region Fields
+
+        private readonly ILoggingService _LoggingService;
+
+        #endregion
+
+        #region Constructors
+
+        public MainPageViewModel(INavigationService navigationService,
+                                 ILoggingService loggingService)
+            : base(navigationService)
+        {
+            this._LoggingService = loggingService;
+            Title = "Main Page";
+        }
+
+        #endregion
+
+        #region Properties
+
+        private DelegateCommand _ShowLogCommand;
+
+        public DelegateCommand ShowLogCommand
+        {
+            get
+            {
+                return this._ShowLogCommand ?? (this._ShowLogCommand = new DelegateCommand(() =>
+                {
+                    this.NavigationService.NavigateAsync("ShowLogs");
+                }));
+            }
+        }
+
+        #endregion
+
+    }
+
+}
