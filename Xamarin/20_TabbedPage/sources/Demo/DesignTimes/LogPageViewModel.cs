5a8838ccd225f86108f40a2f8e3c32240fdf296b
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2023-03-03 01:21:56 +0900

    Add example of using TabPage for Xamarin (#134)

---
+++ Xamarin/20_TabbedPage/sources/Demo/DesignTimes/LogPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Demo.ViewModels.Interfaces;
+using Prism.Commands;
+
+namespace Demo.DesignTimes
+{
+
+    public sealed class LogPageViewModel : ILogPageViewModel
+    {
+
+        public string Title
+        {
+            get;
+        }
+
+    }
+
+}
---
+++ Xamarin/20_TabbedPage/sources/Demo/ViewModels/Interfaces/IMainPageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Prism.Commands;
+
+namespace Demo.ViewModels.Interfaces
+{
+
+    public interface IMainPageViewModel : IViewModel
+    {
+    }
+
+}
