b6f18393e66ccaf4b4bbf918905acc6fd3e1008b
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2023-03-11 22:05:36 +0900

    Add example to use of cpplint (#138)

---
+++ StaticProgramAnalysis/C++/cpplint/01_whitespace/sample.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+void main()  // should almost always be at the end of the previous line
+{
+    std::cout<<"Hello world!!" << std::endl;  // Missing spaces around <<
+}
+// Could not find a newline character at the end of the file
