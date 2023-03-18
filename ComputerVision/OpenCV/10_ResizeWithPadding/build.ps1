683a6b93cb1dba94b5144b74796b320c90de8f2a
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2023-03-18 16:37:50 +0900

    fix

--- ComputerVision/OpenCV/10_ResizeWithPadding/build.ps1
+++ ComputerVision/OpenCV/10_ResizeWithPadding/build.ps1
@@ -38,33 +38,56 @@ $buildDir = Join-Path $current build | `
 $installDir = Join-Path $current install | `
               Join-Path -ChildPath $os
 
-$rootDir = Split-Path $current -Parent
-$openCVInstallDir = Join-Path $rootDir install | `
-                    Join-Path -ChildPath $os | `
-                    Join-Path -ChildPath $target | `
-                    Join-Path -ChildPath $shared | `
-                    Join-Path -ChildPath lib | `
-                    Join-Path -ChildPath cmake | `
-                    Join-Path -ChildPath $target 
-
 New-Item -Type Directory $buildDir -Force | Out-Null
 New-Item -Type Directory $installDir -Force | Out-Null
 
 Push-Location $buildDir
 if ($global:IsWindows)
 {
+    $rootDir = Split-Path $current -Parent
+    $openCVInstallDir = Join-Path $rootDir install | `
+                        Join-Path -ChildPath $os | `
+                        Join-Path -ChildPath $target | `
+                        Join-Path -ChildPath $shared
+    $cmakeModuleFile = Get-ChildItem $openCVInstallDir -Recurse -include OpenCVModules.cmake | Select-Object -First 1
+    if (!($cmakeModuleFile))
+    {
+        Write-Host "OpenCVModules.cmake is missing"
+        exit
+    }
+
+    $openCVInstallDir = Split-Path $cmakeModuleFile -Parent
+
     cmake -D CMAKE_INSTALL_PREFIX=${installDir} `
           -D CMAKE_PREFIX_PATH="${openCVInstallDir}" `
           $sourceDir
 }
 elseif ($global:IsMacOS)
 {
+    $rootDir = Split-Path $current -Parent
+    $openCVInstallDir = Join-Path $rootDir install | `
+                        Join-Path -ChildPath $os | `
+                        Join-Path -ChildPath $target | `
+                        Join-Path -ChildPath $shared | `
+                        Join-Path -ChildPath lib | `
+                        Join-Path -ChildPath cmake | `
+                        Join-Path -ChildPath $target
+
     cmake -D CMAKE_INSTALL_PREFIX=${installDir} `
           -D CMAKE_PREFIX_PATH="${openCVInstallDir}" `
           $sourceDir
 }
 elseif ($global:IsLinux)
 {
+    $rootDir = Split-Path $current -Parent
+    $openCVInstallDir = Join-Path $rootDir install | `
+                        Join-Path -ChildPath $os | `
+                        Join-Path -ChildPath $target | `
+                        Join-Path -ChildPath $shared | `
+                        Join-Path -ChildPath lib | `
+                        Join-Path -ChildPath cmake | `
+                        Join-Path -ChildPath $target
+
     cmake -D CMAKE_INSTALL_PREFIX=${installDir} `
           -D CMAKE_PREFIX_PATH="${openCVInstallDir}" `
           $sourceDir
