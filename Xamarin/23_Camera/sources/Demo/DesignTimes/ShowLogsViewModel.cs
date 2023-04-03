98570ad4d52c7fdd5511eb6a22278d7a0057339a
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2023-04-04 00:38:18 +0900

    fix

---
+++ Xamarin/23_Camera/sources/Demo/DesignTimes/ShowLogsViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Prism.Commands;
+
+using Demo.ViewModels.Interfaces;
+
+namespace Demo.DesignTimes
+{
+
+    public sealed class ShowLogsViewModel : IShowLogsViewModel
+    {
+
+        public DelegateCommand ClearCommand
+        {
+            get;
+        }
+
+        public string Logs
+        {
+            get;
+            set;
+        }
+
+        public string Title
+        {
+            get;
+        }
+
+    }
+
+}
