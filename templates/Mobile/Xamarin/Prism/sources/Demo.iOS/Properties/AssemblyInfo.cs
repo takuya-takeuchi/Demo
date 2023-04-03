a39c270e0e6a5451833e74a896b1660ca6f2e41a
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2023-04-03 22:13:28 +0900

    fix

--- templates/Mobile/Xamarin/Prism/sources/Demo.iOS/Properties/AssemblyInfo.cs
+++ templates/Mobile/Xamarin/Prism/sources/Demo.iOS/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Demo.iOS")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Takuya Takeuchi 2020-2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
