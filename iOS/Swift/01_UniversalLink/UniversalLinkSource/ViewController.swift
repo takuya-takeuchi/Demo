251826a5552ceeb6443859d27a2f09ea82467dc2
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2023-06-03 02:38:59 +0900

    fix

--- iOS/Swift/01_UniversalLink/UniversalLinkSource/ViewController.swift
+++ iOS/Swift/01_UniversalLink/UniversalLinkSource/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonLaunchApp: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    /// Launch App by using Universal Link
+    /// - Parameter sender: 
+    @IBAction func launchApp(_ sender: Any) {
+        let url = URL(string: "https://taktak.jp/buy?key=value")
+        UIApplication.shared.open(url!, options: [.universalLinksOnly : false]) { (success) in
+            if(!success){
+                print("Not a universal link")
+            }
+            else{
+                print("working!!")
+            }
+        }
+    }
 
 }
 
