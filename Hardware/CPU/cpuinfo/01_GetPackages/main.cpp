9275d9097071dad5d024e569fb9b6145c4aaf813
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2023-08-04 02:32:58 +0900

    fix

---
+++ Hardware/CPU/cpuinfo/01_GetPackages/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <cstdlib>
+#include <iostream>
+
+#include <cpuinfo.h>
+
+int32_t main(int32_t argc, const char** argv)
+{
+    if (!cpuinfo_initialize())
+    {
+        std::cout << "[Error] Failed cpuinfo_initialize" << std::endl;
+        return -1;
+    }
+
+    const uint32_t packages_count = cpuinfo_get_packages_count();
+    for (auto index = 0; index < packages_count; index++)
+    {
+        const cpuinfo_package* package = cpuinfo_get_package(0);
+        if (package == nullptr)
+        {
+            std::cout << "[Error] Failed cpuinfo_get_package(" << index << ")" << std::endl;
+            continue;
+        }
+
+        std::cout << "[info] name: " << package->name << std::endl;
+    }
+}
