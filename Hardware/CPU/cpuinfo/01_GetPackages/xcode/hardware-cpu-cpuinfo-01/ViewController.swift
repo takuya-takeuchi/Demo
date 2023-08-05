076099f286878fae24da98d860cc2348590145a1
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2023-08-05 13:00:37 +0900

    fix

---
+++ Hardware/CPU/cpuinfo/01_GetPackages/xcode/hardware-cpu-cpuinfo-01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hardware-cpu-cpuinfo-01
+//
+//  Created by Takuya Takeuchi on 2023/08/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
