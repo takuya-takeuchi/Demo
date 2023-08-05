83947e8a0a8f57d0f00080accb8a8d215b8fc3f4
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2023-08-05 17:38:38 +0900

    fix

---
+++ Hardware/CPU/cpuinfo/01_GetPackages/xamarin/sources/Demo/DesignTimes/ShowLogsPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Prism.Commands;
+
+using Demo.ViewModels.Interfaces;
+
+namespace Demo.DesignTimes
+{
+
+    public sealed class ShowLogsPageViewModel : IShowLogsPageViewModel
+    {
+
+        public DelegateCommand ClearCommand
+        {
+            get;
+        }
+
+        public string Logs
+        {
+            get;
+            set;
+        }
+
+        public string Title
+        {
+            get;
+        }
+
+    }
+
+}
---
+++ Hardware/CPU/cpuinfo/01_GetPackages/xamarin/sources/Demo/ViewModels/Interfaces/IShowLogsPageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Commands;
+
+namespace Demo.ViewModels.Interfaces
+{
+
+    public interface IShowLogsPageViewModel : IViewModel
+    {
+
+        DelegateCommand ClearCommand
+        {
+            get;
+        }
+
+        string Logs
+        {
+            get;
+        }
+
+    }
+
+}
