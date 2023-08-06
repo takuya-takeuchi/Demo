d802581303d05511cd7daef174709f10cb913b50
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2023-08-06 20:27:21 +0900

    fix

---
+++ Hardware/CPU/cpuinfo/01_GetPackages/maui/Demo/PInovke/NativeMethods.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Runtime.InteropServices;
+
+// ReSharper disable once CheckNamespace
+namespace Demo.PInvoke
+{
+
+    internal sealed partial class NativeMethods
+    {
+
+        #region Fields
+
+        private const string NativeLibrary = "cpuinfo";
+
+        [DllImport(NativeLibrary)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool cpuinfo_initialize();
+
+        [DllImport(NativeLibrary)]
+        public static extern uint cpuinfo_get_packages_count();
+
+        [DllImport(NativeLibrary)]
+        public static unsafe extern IntPtr cpuinfo_get_package(uint index);
+
+        #endregion
+
+    }
+
+}
