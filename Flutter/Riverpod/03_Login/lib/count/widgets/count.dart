3b1c2587e6729981f8fdf448c977af655f7a6320
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2023-08-20 22:15:02 +0900

    fix

---
+++ Flutter/Riverpod/03_Login/lib/count/widgets/count.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../providers/counter.dart';
+
+class CountWidget extends ConsumerWidget {
+  const CountWidget({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final count = ref.watch(counterProvider);
+    return Text(
+      '$count',
+      style: Theme.of(context).textTheme.headlineMedium,
+    );
+  }
+}
