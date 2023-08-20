1f2772235773f8dcd22be015bea9f38aa7b06897
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2023-08-21 00:46:54 +0900

    fix

--- Flutter/Riverpod/03_Login/lib/login/screens/login.dart
+++ Flutter/Riverpod/03_Login/lib/login/screens/login.dart
@@ -11,92 +11,99 @@ class LoginScreen extends ConsumerWidget {
     {super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context, WidgetRef ref) {;
+    double screenWidth = MediaQuery.of(context).size.width;
+    double logoWidth = screenWidth * 0.6;
+    double textFieldWidth = screenWidth * 0.7;
+
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const SizedBox(
-              width: 320,
-              child: Image(
-                image: AssetImage('assets/images/logo.png'),
-                fit: BoxFit.cover,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 60, 0, 0),
-              child: SizedBox(
-                width: 400,
-                height: 50,
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'user name',
-                    border: OutlineInputBorder(),
-                  ),
-                  onChanged: (String value) {
-                    ref.read(usernameProvider.notifier).setValue(value);
-                  },
-                  style: const TextStyle(fontSize: 20),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 60, 0, 0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                width: logoWidth,
+                child: const Image(
+                  image: AssetImage('assets/images/logo.png'),
+                  fit: BoxFit.cover,
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 20, 0, 60),
-              child: SizedBox(
-                width: 400,
-                height: 50,
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'password',
-                    border: OutlineInputBorder(),
-                  ),
-                  obscureText: true,
-                  onChanged: (String value) {
-                    ref.read(passwordProvider.notifier).setValue(value);
-                  },
-                  style: const TextStyle(fontSize: 20),
-                ),
-              ),
-            ),
-            SizedBox(
-              width: 400,
-              height: 50,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  textStyle: const TextStyle(fontSize: 20.0),
-                ),
-                onPressed: () async {
-                  var username = ref.read(usernameProvider);
-                  var password = ref.read(passwordProvider);
-                  if (_authenticationService.Login(username, password)) {
-                    Navigator.pushNamed(
-                      context,
-                      '/count',
-                    );
-                    return;
-                  }
-                  
-                  showDialog<String>(
-                    context: context,
-                    builder: (BuildContext context) => AlertDialog(
-                      title: const Text('error'),
-                      content: const Text('failed to login'),
-                      actions: <Widget>[
-                        TextButton(
-                          onPressed: () =>
-                              Navigator.pop(context, 'retry'),
-                          child: const Text('retry'),
-                        ),
-                      ],
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 60, 0, 0),
+                child: SizedBox(
+                  width: textFieldWidth,
+                  height: 50,
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'user name',
+                      border: OutlineInputBorder(),
                     ),
-                  );
-                },
-                child: const Text('Login'),
+                    onChanged: (String value) {
+                      ref.read(usernameProvider.notifier).setValue(value);
+                    },
+                    style: const TextStyle(fontSize: 20),
+                  ),
+                ),
               ),
-            ),
-          ],
-        ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 20, 0, 60),
+                child: SizedBox(
+                  width: textFieldWidth,
+                  height: 50,
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'password',
+                      border: OutlineInputBorder(),
+                    ),
+                    obscureText: true,
+                    onChanged: (String value) {
+                      ref.read(passwordProvider.notifier).setValue(value);
+                    },
+                    style: const TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: textFieldWidth,
+                height: 50,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    textStyle: const TextStyle(fontSize: 20.0),
+                  ),
+                  onPressed: () async {
+                    var username = ref.read(usernameProvider);
+                    var password = ref.read(passwordProvider);
+                    if (_authenticationService.Login(username, password)) {
+                      Navigator.pushNamed(
+                        context,
+                        '/count',
+                      );
+                      return;
+                    }
+                    
+                    showDialog<String>(
+                      context: context,
+                      builder: (BuildContext context) => AlertDialog(
+                        title: const Text('error'),
+                        content: const Text('failed to login'),
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () =>
+                                Navigator.pop(context, 'retry'),
+                            child: const Text('retry'),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                  child: const Text('Login'),
+                ),
+              ),
+            ],
+          ),
+        ),        
       ),
     );
   }
--- Flutter/Riverpod/03_Login/lib/main.dart
+++ Flutter/Riverpod/03_Login/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -17,13 +18,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // mobiel device has native splash screen
+    Widget? initScreen;
+    if (Platform.isAndroid || Platform.isIOS)
+    {
+      initScreen = LoginScreen();
+    }
+    else
+    {
+      initScreen = const SplashScreen();
+    }
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const SplashScreen(),
+      home: initScreen,
       routes: <String, WidgetBuilder>{
         '/splashscreen': (BuildContext context) => const SplashScreen(),
         '/login': (BuildContext context) => LoginScreen(),
