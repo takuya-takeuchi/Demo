ec6f10cc18ed22cb6aac6f05b9d70e231db8b951
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2023-09-16 20:30:16 +0900

    fix

--- Flutter/Vanilla/02_QrCode/demo/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Flutter/Vanilla/02_QrCode/demo/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import mobile_scanner
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
 }
