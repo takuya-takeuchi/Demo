326074ac3f49ea7ab1e0a8225de07f36480508a9
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2023-11-26 20:48:52 +0900

    fix

---
+++ templates/Desktop/Windows Forms/DotNet/Vanilla/Demo/sources/Demo/MainForm.cs
@@ -0,0 +1,20 @@
+using System.Windows.Forms;
+
+namespace Demo
+{
+
+    public partial class MainForm : Form
+    {
+
+        #region Constructors
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+    }
+
+}
