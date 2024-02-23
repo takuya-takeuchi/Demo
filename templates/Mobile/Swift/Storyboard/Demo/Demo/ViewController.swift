00b78d4af74b09e4314abbaa09d7f20116dcbe8e
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2024-02-23 12:54:37 +0900

    fix: add template

---
+++ templates/Mobile/Swift/Storyboard/Demo/Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Takuya Takeuchi on 2024/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
