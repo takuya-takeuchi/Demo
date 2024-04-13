1305c170d08878b34c81e1aa0a02fe833ce57288
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2024-04-13 22:57:21 +0900

    Add example C++ code generated by OpenAPI generator for cppresetsdk (#226)

---
+++ Networking/cpprestsdk/07_OpenAPI/build-openapi.ps1
@@ -0,0 +1,112 @@
+#***************************************
+#Arguments
+#%1: Build Configuration (Release/Debug)
+#***************************************
+Param
+(
+   [Parameter(
+   Mandatory=$True,
+   Position = 1
+   )][string]
+   $Configuration
+)
+
+$current = $PSScriptRoot
+
+# get os name
+if ($global:IsWindows)
+{
+    $os = "win"
+}
+elseif ($global:IsMacOS)
+{
+    $os = "osx"
+}
+elseif ($global:IsLinux)
+{
+    $os = "linux"
+}
+
+$target = "openapi"
+
+# build
+$sourceDir = Join-Path $current $target 
+$buildDir = Join-Path $current $target | `
+            Join-Path -ChildPath build | `
+            Join-Path -ChildPath $os | `
+            Join-Path -ChildPath program
+$installDir = Join-Path $current install | `
+              Join-Path -ChildPath $os
+
+$targetDir = Join-Path $installDir $target | `
+             Join-Path -ChildPath lib | `
+             Join-Path -ChildPath cmake
+
+New-Item -Type Directory $buildDir -Force | Out-Null
+New-Item -Type Directory $installDir -Force | Out-Null
+
+Push-Location $buildDir
+if ($global:IsWindows)
+{
+    if (!($env:VCPKG_ROOT_DIR))
+    {
+        Write-Host "VCPKG_ROOT_DIR environmental variable is missing" -ForegroundColor Red
+        return
+    }
+
+    $toolchain = "${env:VCPKG_ROOT_DIR}\scripts\buildsystems\vcpkg.cmake"
+    if (!(Test-Path(${toolchain})))
+    {
+        Write-Host "${toolchain} is missing" -ForegroundColor Red
+        return
+    }
+
+    cmake -D CMAKE_INSTALL_PREFIX=${installDir} `
+          -D CMAKE_PREFIX_PATH="${targetDir}" `
+          -D CMAKE_TOOLCHAIN_FILE="${env:VCPKG_ROOT_DIR}\scripts\buildsystems\vcpkg.cmake" `
+          -D VCPKG_TARGET_TRIPLET="x64-windows-static" `
+          -D BUILD_SHARED_LIBS=OFF `
+          $sourceDir
+}
+elseif ($global:IsMacOS)
+{
+    cmake -D CMAKE_INSTALL_PREFIX=${installDir} `
+          -D CMAKE_PREFIX_PATH="${targetDir}" `
+          -D OPENSSL_ROOT_DIR=/usr/local/opt/openssl `
+          -D OPENSSL_LIBRARIES=/usr/local/opt/openssl/lib `
+          -D BUILD_SHARED_LIBS=OFF `
+          $sourceDir
+}
+elseif ($global:IsLinux)
+{
+    cmake -D CMAKE_INSTALL_PREFIX=${installDir} `
+          -D CMAKE_PREFIX_PATH="${targetDir}" `
+          -D BUILD_SHARED_LIBS=OFF `
+          $sourceDir
+}
+cmake --build . --config ${Configuration} --target install
+Pop-Location
+
+# run
+if ($global:IsWindows)
+{
+    $programDir = Join-Path $current build | `
+                  Join-Path -ChildPath $os | `
+                  Join-Path -ChildPath program | `
+                  Join-Path -ChildPath ${Configuration}
+    $program = Join-Path $programDir Test.exe
+}
+elseif ($global:IsMacOS)
+{
+    $programDir = Join-Path $current build | `
+                  Join-Path -ChildPath $os | `
+                  Join-Path -ChildPath program
+    $program = Join-Path $programDir Test
+}
+elseif ($global:IsLinux)
+{
+    $programDir = Join-Path $current build | `
+                  Join-Path -ChildPath $os | `
+                  Join-Path -ChildPath program
+    $program = Join-Path $programDir Test
+}
---
+++ Networking/cpprestsdk/07_OpenAPI/main.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <memory>
+#include <vector>
+
+#include <OpenAPIClient/ApiClient.h>
+#include <OpenAPIClient/api/MessageApi.h>
+
+int32_t main(int32_t argc, const char** argv)
+{
+    const auto url = std::string(argv[1]);
+
+    try
+    {
+        const auto apiConfiguration = std::make_shared<demo::client::api::ApiConfiguration>();
+        apiConfiguration->setBaseUrl(::utility::conversions::to_string_t(url));
+        const auto apiClient = std::make_shared<demo::client::api::ApiClient>(apiConfiguration);
+        const auto messageApi = std::make_shared<demo::client::api::MessageApi>(apiClient);
+        const auto ret = messageApi->apiGetMessageGet().then([=](pplx::task<std::shared_ptr<demo::client::model::Message>> message) {
+            const auto date = message.get()->getDate().to_string(utility::datetime::ISO_8601);
+            const auto dateStr = utility::conversions::to_utf8string(date);
+            const auto text = message.get()->getText();
+            const auto textStr = utility::conversions::to_utf8string(text);
+            std::cout << "[Info] message: " << textStr << std::endl;
+            std::cout << "[Info]    date: " << dateStr << std::endl;
+            // std::cout << "[Info]    date: " << date.year() << "/" << date.month() << "/"  << date.day() << std::endl;
+        }).wait();
+    }
+    catch (const std::exception& e)
+    {
+        std::cout << "[Error] Exception: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
