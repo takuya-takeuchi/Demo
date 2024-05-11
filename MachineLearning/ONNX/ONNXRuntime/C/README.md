abb02819906d6ff861a053f1b6e46c4758f61466
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2024-05-12 08:53:18 +0900

    fix

---
+++ MachineLearning/ONNX/ONNXRuntime/C/README.md
@@ -0,0 +1,37 @@
+# ONNX Runtime (C API)
+
+## Abstracts
+
+* Build ONNX Runtime
+
+## Requirements
+
+### Common
+
+* Powershell 7 or later
+* CMake
+  * 3.26 or higher
+
+### Windows
+
+* Visual Studio 2022
+
+### Ubuntu
+
+* g++
+
+### OSX
+
+* Xcode
+
+## Dependencies
+
+* [ONNX Runtime](https://onnxruntime.ai/)
+  * 1.16.3
+  * MIT license
+
+## How to use?
+
+````shell
+$ pwsh build.ps1  <Debug/Release>
+````
--- MachineLearning/ONNX/ONNXRuntime/C/build.ps1
+++ MachineLearning/ONNX/ONNXRuntime/C/build.ps1
@@ -28,14 +28,12 @@ elseif ($global:IsLinux)
 }
 
 $target = "onnxruntime"
-$shared = "static"
 
 # build
 $sourceDir = Join-Path $current $target
 $buildDir = Join-Path $current build | `
             Join-Path -ChildPath $os | `
-            Join-Path -ChildPath $target | `
-            Join-Path -ChildPath $Configuration
+            Join-Path -ChildPath $target
 $installDir = Join-Path $current install | `
               Join-Path -ChildPath $os | `
               Join-Path -ChildPath $target | `
@@ -48,15 +46,57 @@ New-Item -Type Directory $buildDir -Force | Out-Null
 New-Item -Type Directory $installDir -Force | Out-Null
 
 Push-Location $target
+
+git submodule update --init --recursive .
+
 if ($global:IsWindows)
-{    
+{
     python tools/ci_build/build.py --config ${Configuration} `
                                    --cmake_generator "Visual Studio 17 2022" `
                                    --parallel `
-                                   --x86 `
                                    --build_dir ${buildDir} `
                                    --skip_tests `
-                                   --use_full_protobuf
+                                   --use_full_protobuf `
+                                   --cmake_extra_defines CMAKE_INSTALL_PREFIX=$installDir
+
+    $artifactDir = Join-Path $buildDir $Configuration
+    cmake --install $artifactDir --config ${Configuration}
+
+    $deps = @()
+    $deps += New-Object PSObject -Property @{ Name = "abseil";          Target = "abseil_cpp-build\absl\base"; }
+    $deps += New-Object PSObject -Property @{ Name = "abseil";          Target = "abseil_cpp-build\absl\container"; }
+    $deps += New-Object PSObject -Property @{ Name = "abseil";          Target = "abseil_cpp-build\absl\debugging"; }
+    $deps += New-Object PSObject -Property @{ Name = "abseil";          Target = "abseil_cpp-build\absl\hash"; }
+    $deps += New-Object PSObject -Property @{ Name = "abseil";          Target = "abseil_cpp-build\absl\numeric"; }
+    $deps += New-Object PSObject -Property @{ Name = "abseil";          Target = "abseil_cpp-build\absl\profiling"; }
+    $deps += New-Object PSObject -Property @{ Name = "abseil";          Target = "abseil_cpp-build\absl\strings"; }
+    $deps += New-Object PSObject -Property @{ Name = "abseil";          Target = "abseil_cpp-build\absl\synchronization"; }
+    $deps += New-Object PSObject -Property @{ Name = "abseil";          Target = "abseil_cpp-build\absl\time"; }
+    $deps += New-Object PSObject -Property @{ Name = "abseil";          Target = "abseil_cpp-build\absl\types"; }
+    $deps += New-Object PSObject -Property @{ Name = "flatbuffers";     Target = "flatbuffers-build"; }
+    $deps += New-Object PSObject -Property @{ Name = "onnx";            Target = "onnx-build"; }
+    $deps += New-Object PSObject -Property @{ Name = "protobuf";        Target = "protobuf-build"; }
+    $deps += New-Object PSObject -Property @{ Name = "pytorch_cpuinfo"; Target = "pytorch_cpuinfo-build"; }
+    $deps += New-Object PSObject -Property @{ Name = "re2";             Target = "re2-build"; }
+    $exts = @(
+        "*.lib"
+        "*.pdb"
+    )
+
+    $depsInstallDir = Join-Path $installDir lib | Join-Path -ChildPath deps
+    $depsDir = Join-Path $artifactDir _deps
+    foreach ($dep in $deps)
+    {
+        $depSourceDir = Join-Path $depsDir $dep.Target | Join-Path -ChildPath $Configuration
+        $depsDestDir = Join-Path $depsInstallDir $dep.Name
+        New-Item -Type Directory $depsDestDir -Force | Out-Null
+
+        foreach ($ext in $exts)
+        {
+            $src = Join-Path $depSourceDir $ext
+            Copy-Item "${src}" "${depsDestDir}" -Force
+        }
+    }
 }
 elseif ($global:IsMacOS)
 {
@@ -110,5 +150,4 @@ elseif ($global:IsLinux)
           -D WITH_TIFF=ON `
           $sourceDir
 }
-cmake --build . --config ${Configuration} --target install
 Pop-Location
