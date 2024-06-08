577c3316a21984f9c671845adfafb9fa6182ccfd
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2024-06-08 19:08:33 +0900

    fix

--- Misc/23_TemplateViaTemplate/main.cpp
+++ Misc/23_TemplateViaTemplate/main.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
 
-template<typename T>
 class sample
 {
     public:
-        void hello(T value)
+        template<int T>
+        void hello()
         {
-            std::cout << value << std::endl;
+            std::cout << T << std::endl;
         }
 };
 
 template<typename T>
-void test(T value)
+void test()
 {
-    sample<T> s;
-    s.hello(value);
+    T s;
+#ifdef _WINDOWS
+    // or s.template hello<10>();
+    s.hello<10>();
+#else
+    // s.hello<10>(); // gcc says ' error: invalid operands of types ‘<unresolved overloaded function type>’ and ‘int’ to binary ‘operator<’'
+    s.template hello<10>();
+#endif
 }
 
 int main ()
 {
-    test(10);
+    test<sample>();
 }
