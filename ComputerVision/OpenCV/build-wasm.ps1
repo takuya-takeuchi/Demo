d7db6fb6104f46f276b82e30a1ecc1a158c561df
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2024-08-07 00:08:04 +0900

    fix

--- ComputerVision/OpenCV/build-wasm.ps1
+++ ComputerVision/OpenCV/build-wasm.ps1
@@ -1,16 +1,3 @@
-#***************************************
-#Arguments
-#%1: Build Configuration (Release/Debug)
-#***************************************
-Param
-(
-   [Parameter(
-   Mandatory=$True,
-   Position = 1
-   )][string]
-   $Configuration
-)
-
 $current = $PSScriptRoot
 
 # get os name
@@ -33,76 +20,81 @@ $sharedFlag = "OFF"
 
 # --memory-init-file is not supported 3.1.55 or later
 # This issue is fixed by https://github.com/opencv/opencv/pull/25629
-# $emsdkVersion = "3.1.64"
+# $emSdkVersion = "3.1.64"
 
 # cv.imread is not a function
 # https://github.com/opencv/opencv/issues/25057
 # https://github.com/opencv/opencv/issues/21580
 # https://github.com/opencv/opencv/issues/24620
-# $emsdkVersion = "3.1.51"
+# $emSdkVersion = "3.1.51"
 
 # cv.imread is not a function
-# $emsdkVersion = "3.0.1"
+# $emSdkVersion = "3.0.1"
 
 # https://github.com/opencv/opencv/pull/25084
-# $emsdkVersion = "3.1.54"
+# $emSdkVersion = "3.1.54"
 
 # https://docs.opencv.org/4.x/d4/da1/tutorial_js_setup.html
 # Mac is not supported
 # cv.imread is not a function
-# $emsdkVersion = "2.0.10"
+# $emSdkVersion = "2.0.10"
 
 # cv.imread is not a function
 # https://stackoverflow.com/questions/67190799/how-to-include-cv-imread-when-building-opencv-js
-# $emsdkVersion = "1.39.15"# OK
-$emsdkVersion = "1.39.16" # OK
-# $emsdkVersion = "1.39.17" # not work!!
-# $emsdkVersion = "1.39.18" # not work!!
-# $emsdkVersion = "1.39.19" # not work!!
-# $emsdkVersion = "1.39.20" # not work!!
-# $emsdkVersion = "1.40.1" # not work!!
-
-# build
-$sourceDir = Join-Path $current $target
-$buildDirName = "build-wasm"
-$buildDir = Join-Path $current ${buildDirName} | `
-            Join-Path -ChildPath $os | `
-            Join-Path -ChildPath $target | `
-            Join-Path -ChildPath $shared
-$installDir = Join-Path $current install-wasm | `
-              Join-Path -ChildPath $os | `
-              Join-Path -ChildPath $target | `
-              Join-Path -ChildPath $shared
-$targetDir = Join-Path $installDir $target | `
-             Join-Path -ChildPath lib | `
-             Join-Path -ChildPath cmake
-
-New-Item -Type Directory $buildDir -Force | Out-Null
-New-Item -Type Directory $installDir -Force | Out-Null
-
-$emsdkDir = Join-Path $current emsdk
-$buildPythonScript = Join-Path $target platform | `
-                     Join-Path -ChildPath js | `
-                     Join-Path -ChildPath build_js.py
+$emSdkVersion = "1.39.15" # OK
+$emsdkVersions = @(
+    "1.39.15" # OK
+    "1.39.16" # not work!!
+    "1.39.17" # not work!!
+    "1.39.18" # not work!!
+    "1.39.19" # not work!!
+    "1.39.20" # not work!!
+    "1.40.1"  # not work!!
+)
 
 git submodule update --init --recursive .
 
-Push-Location $buildDir
-
-
-$dockerBuildDir = Join-Path ${buildDirName} $os | `
+foreach ($emSdkVersion in $emsdkVersions)
+{
+    # build
+    $sourceDir = Join-Path $current $target
+    $buildDirName = "build-wasm"
+    $buildDir = Join-Path $current ${buildDirName} | `
+                Join-Path -ChildPath $emSdkVersion | `
+                Join-Path -ChildPath $os | `
+                Join-Path -ChildPath $target | `
+                Join-Path -ChildPath $shared
+    $installDir = Join-Path $current install-wasm | `
+                  Join-Path -ChildPath $emSdkVersion | `
+                  Join-Path -ChildPath $os | `
                   Join-Path -ChildPath $target | `
                   Join-Path -ChildPath $shared
-
-if ($global:IsWindows)
-{
-    $dockerBuildDir = $dockerBuildDir.Replace("`\", "/")
+    $targetDir = Join-Path $installDir $target | `
+                 Join-Path -ChildPath lib | `
+                 Join-Path -ChildPath cmake
+    
+    New-Item -Type Directory $buildDir -Force | Out-Null
+    New-Item -Type Directory $installDir -Force | Out-Null
+    
+    $buildPythonScript = Join-Path $target platform | `
+                         Join-Path -ChildPath js | `
+                         Join-Path -ChildPath build_js.py    
+        
+    $dockerBuildDir = Join-Path ${buildDirName} $emSdkVersion | `
+                      Join-Path -ChildPath $os | `
+                      Join-Path -ChildPath $target | `
+                      Join-Path -ChildPath $shared
+    
+    if ($global:IsWindows)
+    {
+        $dockerBuildDir = $dockerBuildDir.Replace("`\", "/")
+    }
+    
+    # build
+    docker run --rm --workdir /project/${target} -v "${current}:/project" "emscripten/emsdk:${emSdkVersion}" emcmake python3 ./platforms/js/build_js.py "/project/${dockerBuildDir}"
+    
+    # copy opencv.js
+    $installDir = Join-Path $installDir bin
+    New-Item -Type Directory $installDir -Force | Out-Null
+    Copy-Item (Join-Path $buildDir bin | Join-Path -ChildPath "*") $installDir -Force -Recurse | Out-Null
 }
-
-# build
-docker run --rm --workdir /project/${target} -v "${current}:/project" "emscripten/emsdk:${emsdkVersion}" emcmake python3 ./platforms/js/build_js.py "/project/${dockerBuildDir}"
-
-# copy opencv.js
-$installDir = Join-Path $installDir bin
-New-Item -Type Directory $installDir -Force | Out-Null
-Copy-Item (Join-Path $buildDir bin | Join-Path -ChildPath "*") $installDir -Force -Recurse | Out-Null
