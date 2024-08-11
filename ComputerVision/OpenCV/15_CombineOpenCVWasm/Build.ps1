4b845fb146609b63d7be0231ba62e8698a703313
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2024-08-12 01:23:10 +0900

    fix

--- ComputerVision/OpenCV/15_CombineOpenCVWasm/Build.ps1
+++ ComputerVision/OpenCV/15_CombineOpenCVWasm/Build.ps1
@@ -31,9 +31,12 @@ $target = "opencv4"
 $shared = "static"
 $emSdkVersion = "1.39.15"
 $emSdkVersion = "2.0.22"
+
 # Not work!!
-# https://github.com/atteneder/DracoUnity/issues/55
-# error: undefined symbol: _ZNSt3__212basic_stringIcNS_11char_traitsIcEENS_9allocatorIcEEEC1ERKS5_ (referenced by top-level compiled C/C++ code)
+# * https://github.com/atteneder/DracoUnity/issues/55
+#   error: undefined symbol: _ZNSt3__212basic_stringIcNS_11char_traitsIcEENS_9allocatorIcEEEC1ERKS5_ (referenced by top-level compiled C/C++ code)
+# * from 2.0.23, emscripten updated llvm components to llvm-12
+#   Sedd https://github.com/emscripten-core/emscripten/blob/main/ChangeLog.md#2023---05262021
 # $emSdkVersion = "2.0.23"
 # $emSdkVersion = "2.0.24"
 # $emSdkVersion = "2.0.25"
@@ -48,7 +51,7 @@ $emSdkVersion = "2.0.22"
 # $emSdkVersion = "2.0.34"
 
 # Not work!!
-# wasm-ld: error: /project/install-wasm/1.39.15/win/opencv4/static/lib/libopencv_core.a(system.cpp.o): undefined symbol: std::__2::__basic_string_common<true>::__throw_length_error() const
+# * wasm-ld: error: /project/install-wasm/1.39.15/win/opencv4/static/lib/libopencv_core.a(system.cpp.o): undefined symbol: std::__2::__basic_string_common<true>::__throw_length_error() const
 # $emSdkVersion = "3.1.64"
 # $emSdkVersion = "3.1.57"
 $opencvEmSdkVersion = "1.39.15"
