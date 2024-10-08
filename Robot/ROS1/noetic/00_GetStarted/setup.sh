998e26fcfecdf58479ac77133555bc3d8f7baee6
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2024-10-09 01:30:55 +0900

    fix

--- Robot/ROS1/noetic/00_GetStarted/setup.sh
+++ Robot/ROS1/noetic/00_GetStarted/setup.sh
@@ -3,8 +3,11 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 mkdir -p ${SCRIPT_DIR}/catkin_ws/src
-cd ${SCRIPT_DIR}/catkin_ws
-catkin build
 
-# echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
-# source ~/.bashrc
+pushd ${SCRIPT_DIR}/catkin_ws/src
+catkin_create_pkg hello std_msgs roscpp
+popd
+
+pushd ${SCRIPT_DIR}/catkin_ws
+catkin build
+popd
