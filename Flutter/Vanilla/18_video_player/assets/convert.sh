28f5a82f3a4caea9e44ca54dcae501ee571d3659
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2025-02-23 14:42:21 +0900

    fix

--- Flutter/Vanilla/18_video_player/assets/convert.sh
+++ Flutter/Vanilla/18_video_player/assets/convert.sh
@@ -1,7 +1,4 @@
 #!/bin/bash +x
 
 FILE="file_example_WEBM_1920_3_7MB"
-
-# iOS simulator and OSX can not play it if `-profile:v high -pix_fmt yuv420p` is not specified
-# ffmpeg -y -i $FILE.webm -an $FILE.mp4
-ffmpeg -y -i $FILE.webm -c:v libx264 -profile:v high -pix_fmt yuv420p -r 30 -an ${FILE}.mp4
+ffmpeg -y -i $FILE.webm -an ${FILE}.mp4
