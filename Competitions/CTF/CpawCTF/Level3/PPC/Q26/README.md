53921a8d929e67a4209c6e2c2c1c060ae3236f6e
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2025-03-23 22:30:15 +0900

    fix

--- Competitions/CTF/CpawCTF/Level3/PPC/Q26/README.md
+++ Competitions/CTF/CpawCTF/Level3/PPC/Q26/README.md
@@ -2,3 +2,9 @@
 
 ## How to resolve?
 
+You need to find value `x` which satisfies the following statements.
+
+* x mod 1584891 = 32134
+* x mod 3438478 = 193127
+
+In other words?
--- Competitions/CTF/CpawCTF/Level3/PPC/Q26/solver.py
+++ Competitions/CTF/CpawCTF/Level3/PPC/Q26/solver.py
@@ -1,14 +1,23 @@
 # -*- coding: utf-8 -*-
 
+from numba import njit
+
 print(f"Find satisfies both math formula.")
 print(f"\tx ≡ 32134 (mod 1584891)")
 print(f"\tx ≡ 193127 (mod 3438478)")
 
 print("start find....")
-k = 1
-while(True):
-    x = 193127 * 3438478 * k
-    if x % 1584891 == 32134:
-        print(f"x = {x}")
-        break
-    k += 1
+
+@njit
+def task():
+    k = 1
+    while(True):
+        x = 193127 + 3438478 * k
+        if x % 1584891 == 32134:
+            return x
+        k += 1
+
+x = task()
+print(f"x = {x}")
+print(f"x mod 1584891 = {x % 1584891}")
+print(f"x mod 3438478 = {x % 3438478}")
