194a58fd5a5b9a1c1941466da8c0a5dbf48a14da
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2025-07-20 11:55:06 +0900

    fix

---
+++ Misc/oneTBB/build.ps1
@@ -0,0 +1,78 @@
+#***************************************
+#Arguments
+#%1: Build Configuration (Release/Debug)
+#***************************************
+Param
+(
+   [Parameter(
+   Mandatory=$True,
+   Position = 1
+   )][string]
+   $Configuration
+)
+
+$current = $PSScriptRoot
+
+# get os name
+if ($global:IsWindows)
+{
+    $os = "win"
+}
+elseif ($global:IsMacOS)
+{
+    $os = "osx"
+}
+elseif ($global:IsLinux)
+{
+    $os = "linux"
+}
+
+$target = "oneTBB"
+# For onTBB, Static library is highly discouraged and such configuration is not supported.
+$shared = "dynamic"
+$sharedFlag = "ON"
+$tbbVerifyDependencySignature = "OFF"
+
+# build
+$sourceDir = Join-Path $current $target
+$buildDir = Join-Path $current build | `
+            Join-Path -ChildPath $os | `
+            Join-Path -ChildPath $target | `
+            Join-Path -ChildPath $shared
+$installDir = Join-Path $current install | `
+              Join-Path -ChildPath $os | `
+              Join-Path -ChildPath $target | `
+              Join-Path -ChildPath $shared
+
+New-Item -Type Directory $buildDir -Force | Out-Null
+New-Item -Type Directory $installDir -Force | Out-Null
+
+git submodule update --init --recursive .
+
+Push-Location $buildDir
+if ($global:IsWindows)
+{
+    cmake -D CMAKE_INSTALL_PREFIX=${installDir} `
+          -D CMAKE_BUILD_TYPE=$Configuration `
+          -D BUILD_SHARED_LIBS=${sharedFlag} `
+          -D TBB_VERIFY_DEPENDENCY_SIGNATURE=${tbbVerifyDependencySignature} `
+          $sourceDir
+}
+elseif ($global:IsMacOS)
+{
+    cmake -D CMAKE_INSTALL_PREFIX=${installDir} `
+          -D CMAKE_BUILD_TYPE=$Configuration `
+          -D BUILD_SHARED_LIBS=${sharedFlag} `
+          -D TBB_VERIFY_DEPENDENCY_SIGNATURE=${tbbVerifyDependencySignature} `
+          $sourceDir
+}
+elseif ($global:IsLinux)
+{
+    cmake -D CMAKE_INSTALL_PREFIX=${installDir} `
+          -D CMAKE_BUILD_TYPE=$Configuration `
+          -D BUILD_SHARED_LIBS=${sharedFlag} `
+          -D TBB_VERIFY_DEPENDENCY_SIGNATURE=${tbbVerifyDependencySignature} `
+          $sourceDir
+}
+cmake --build . --config ${Configuration} --target install
+Pop-Location
