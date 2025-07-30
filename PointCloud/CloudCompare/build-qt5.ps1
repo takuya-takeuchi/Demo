3f3f93d8125da133af0e2e3ac6de9ffffa9e07a4
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2025-07-31 02:05:15 +0900

    fix

--- PointCloud/CloudCompare/build-qt5.ps1
+++ PointCloud/CloudCompare/build-qt5.ps1
@@ -44,12 +44,74 @@ New-Item -Type Directory $installDir -Force | Out-Null
 
 git submodule update --init --recursive .
 
-Push-Location $buildDir
 if ($global:IsWindows)
 {
-    cmake -D CMAKE_INSTALL_PREFIX=${installDir} `
-          -D CMAKE_BUILD_TYPE=$Configuration `
-          $sourceDir
+    # Check Visual Studio is installed or not
+    $visualStudioShells = @(
+        "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat"
+        "C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvarsall.bat"
+        "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat"
+    )
+    
+    $visualStudioShell = ""
+    foreach ($shell in $visualStudioShells)
+    {
+        $visualStudioShell = $shell
+        if (Test-Path($visualStudioShell))
+        {
+            break
+        }
+    }
+    
+    if (!(Test-Path($visualStudioShell)))
+    {
+        Write-Host "[Error] Visual Studio is not installed or x64 Native Tools Command Prompt is missing" -ForegroundColor Red
+        exit
+    }
+    
+    Write-Host "[Info] Visual Studio x64 Native Tools Command Prompt: ${visualStudioShell}" -ForegroundColor Green
+
+    function Call($batfile)
+    {
+        cmd.exe /c "call `"${batfile}`" && set > %temp%\vars.txt"
+        Get-Content "${env:temp}\vars.txt" | Foreach-Object {
+            if ($_ -match "^(.*?)=(.*)$") {
+                Set-Content "env:\$($matches[1])" $matches[2]
+            }
+        }
+    }
+    
+    $env:_ROOT="${sourceDir}"
+    $env:PATH="$env:_ROOT\qtbase\bin;$env:_ROOT\gnuwin32\bin;$env:PATH"
+    $env:PATH="$env:_ROOT\qtrepotools\bin;$env:PATH"
+
+    # Uncomment the below line when building with OpenSSL enabled. If so, make sure the directory points
+    # to the correct location (binaries for OpenSSL).
+    # $env:PATH="C:\OpenSSL-Win32\bin;$env:PATH"
+
+    # When compiling with ICU, uncomment the lines below and change <icupath> appropriately:
+    # $env:INCLUDE="/path/to/icu/include;$env:INCLUDE"
+    # $env:LIB="/path/to/icu/lib;$env:LIB"
+    # $env:PATH="/path/to/icu/lib;$env:PATH"
+
+    $env:_ROOT=""
+
+    git -C "${sourceDir}" submodule update --init --recursive
+    # cp patch/linux/qt5/qtbase/src/corelib/global/qglobal.h qt5/qtbase/src/corelib/global/qglobal.h
+    
+    Push-Location $buildDir
+    ## build ad lgpl (-confirm-license flags)
+    $configure = Join-Path $sourceDir configure
+    if ($Configuration -eq "Release")
+    {
+        & "${configure}" -developer-build -opensource -nomake examples -nomake tests -confirm-license -release
+    }
+    else
+    {
+        & "${configure}" -developer-build -opensource -nomake examples -nomake tests -confirm-license
+    }
+    nmake
+    Pop-Location
 }
 elseif ($global:IsMacOS)
 {
@@ -64,5 +126,3 @@ elseif ($global:IsLinux)
           -D CMAKE_BUILD_TYPE=$Configuration `
           $sourceDir
 }
-cmake --build . --config ${Configuration} --target install
-Pop-Location
