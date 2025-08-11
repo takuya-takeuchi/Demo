fdba8f37da655390135cceea0668f5f014570eb3
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2025-08-11 23:56:17 +0900

    Sort out UWP template (#321)

---
+++ templates/Desktop/Universal Windows Platform/Prism/README.md
@@ -0,0 +1,24 @@
+# Demo
+
+## Abstracts
+
+* abstracts
+
+## Requirements
+
+* Visual Studio 2022
+
+## Dependencies
+
+* [Microsoft.NETCore.UniversalWindowsPlatform](https://www.nuget.org/packages/microsoft.netcore.universalwindowsplatform)
+  * 6.2.13
+  * Microsoft Software License
+* [Microsoft.Xaml.Behaviors.Uwp.Managed](https://github.com/Microsoft/XamlBehaviors)
+  * 3.0.0
+  * MIT License
+* [Prism.Unity](https://github.com/PrismLibrary/Prism)
+  * 6.3.0
+  * MIT License
+
+## Screenshots
+
---
+++ templates/Desktop/Universal Windows Platform/Prism/sources/Demo/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Prism.Windows.Mvvm;
+
+namespace Demo.ViewModels
+{
+
+    internal sealed class MainPageViewModel : ViewModelBase
+    {
+
+        #region Constructors
+
+        public MainPageViewModel()
+        {
+            this.Text = "Demo";
+        }
+
+        #endregion
+
+        #region Properties
+
+        private string _Text;
+
+        public string Text
+        {
+            get => this._Text;
+            set => this.SetProperty(ref this._Text, value);
+        }
+
+        #endregion
+
+    }
+
+}
