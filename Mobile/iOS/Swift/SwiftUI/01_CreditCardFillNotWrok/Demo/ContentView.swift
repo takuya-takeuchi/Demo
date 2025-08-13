b39c1d8ce670a6d2ce1447ceaa5bdf2a03f0b973
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2025-08-13 11:25:51 +0900

    fix

---
+++ Mobile/iOS/Swift/SwiftUI/01_CreditCardFillNotWrok/Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Demo
+//
+//  Created by Takuya Takeuchi on 2024/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mobile/iOS/Swift/SwiftUI/01_CreditCardFillNotWrok/Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Takuya Takeuchi on 2024/02/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
