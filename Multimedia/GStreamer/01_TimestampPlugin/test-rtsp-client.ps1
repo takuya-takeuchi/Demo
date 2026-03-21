fd4857877e7cf646fb097eeb35688e6b81049f4c
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2026-03-22 00:57:33 +0900

    fix

--- Multimedia/GStreamer/01_TimestampPlugin/test-rtsp-client.ps1
+++ Multimedia/GStreamer/01_TimestampPlugin/test-rtsp-client.ps1
@@ -74,12 +74,12 @@ if (!(Test-Path(${targetBuildDir})))
 if ($global:IsWindows)
 {
     $installBinaryDir = Join-Path $installDir bin
-    $testRecord = Join-Path $targetBuildDir test-launch.exe
+    $gstLaunch = Join-Path $targetInstallDir bin | Join-Path -ChildPath gst-launch-1.0.exe
 }
 elseif ($global:IsMacOS)
 {
     $installBinaryDir = Join-Path $installDir lib
-    $testRecord = Join-Path $targetBuildDir test-launch
+    $gstLaunch = Join-Path $targetInstallDir bin | Join-Path -ChildPath gst-launch-1.0
 }
 elseif ($global:IsLinux)
 {
@@ -87,27 +87,14 @@ elseif ($global:IsLinux)
     $env:GST_PLUGIN_SCANNER="../install/linux/gstreamer/${version}/Release/libexec/gstreamer-1.0/gst-plugin-scanner"
     $env:GST_PLUGIN_SYSTEM_PATH="../install/linux/gstreamer/${version}/Release/lib/x86_64-linux-gnu/gstreamer-1.0"
     $env:LD_LIBRARY_PATH="../install/linux/gstreamer/${version}/Release/lib/x86_64-linux-gnu:${LD_LIBRARY_PATH}"
-    $testRecord = Join-Path $targetBuildDir test-launch
+    $gstLaunch = Join-Path $targetInstallDir bin | Join-Path -ChildPath gst-launch-1.0
 }
 
 $env:GST_DEBUG_DUMP_DOT_DIR=Join-Path $current dot
 $env:GST_PLUGIN_PATH="${installBinaryDir}"
-& $testRecord `
-"( rtph264depay name=depay0 ! h264parse ! tee name=t `
-   t. ! queue ! video/x-h264,alignment=au ! h264autimestampdump location=timestamps_rtsp.csv ! fakesink sync=false `
-   t. ! queue ! h264parse ! mp4mux ! filesink location=output.mp4 )"
-
-# you can change it!!
-$sei_user_data="09452e60-e626-c69e-a7ad-5ad265449e64"
-
 & $gstLaunch -e `
-  filesrc location=output.mp4 ! `
+  filesrc location=input.mp4 ! `
   qtdemux name=d `
   d.video_0 ! queue ! `
   h264parse ! `
-  video/x-h264,alignment=au ! `
-  h264auseifromfile location=timestamps.csv uuid=${sei_user_data} ! `
-  h264seiinserter ! `
-  h264parse ! `
-  mp4mux ! `
-  filesink location=output.with_sei.mp4
+  rtspclientsink location=rtsp://192.168.11.102:8554/test
