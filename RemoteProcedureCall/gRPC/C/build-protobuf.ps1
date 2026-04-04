f8a3d40d593fd8708eea7d51d1da9ec8a3ba4fc5
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2026-04-04 17:19:58 +0900

    fix

---
+++ RemoteProcedureCall/gRPC/C/build-protobuf.ps1
@@ -0,0 +1,143 @@
+#***************************************
+#Arguments
+#%1: Build Configuration (Release/Debug)
+#***************************************
+Param
+(
+   [Parameter(
+   Mandatory=$True,
+   Position = 1
+   )][string]
+   $Configuration
+)
+
+$current = $PSScriptRoot
+$copnfigPath = Join-Path $current "build-config.json"
+if (!(Test-Path($copnfigPath)))
+{
+    Write-Host "${copnfigPath} is missing" -ForegroundColor Red
+    exit
+}
+
+$config = Get-Content -Path $copnfigPath | ConvertFrom-Json
+$target = "protobuf"
+$version = $config.protobuf.version
+if ($config.protobuf.shared)
+{
+    $shared = "dynamic"
+    $sharedFlag = "ON"
+}
+else
+{
+    $shared = "static"
+    $sharedFlag = "OFF"
+}
+
+# get os name
+if ($global:IsWindows)
+{
+    $os = "win"
+}
+elseif ($global:IsMacOS)
+{
+    $os = "osx"
+}
+elseif ($global:IsLinux)
+{
+    $os = "linux"
+}
+
+# build
+$sourceDir = Join-Path $current $target
+$buildDir = Join-Path $current build | `
+            Join-Path -ChildPath $os | `
+            Join-Path -ChildPath $target | `
+            Join-Path -ChildPath $version | `
+            Join-Path -ChildPath $shared | `
+            Join-Path -ChildPath $Configuration
+$installDir = Join-Path $current install | `
+              Join-Path -ChildPath $os | `
+              Join-Path -ChildPath $target | `
+              Join-Path -ChildPath $version | `
+              Join-Path -ChildPath $shared | `
+              Join-Path -ChildPath $Configuration
+
+New-Item -Type Directory $buildDir -Force | Out-Null
+New-Item -Type Directory $installDir -Force | Out-Null
+
+Push-Location $target
+
+git fetch -ap
+git checkout $version
+git submodule update --init --recursive .
+exit
+
+if ($global:IsWindows)
+{
+    function CallVisualStudioDeveloperConsole()
+    {
+        $vs = "C:\Program Files\Microsoft Visual Studio\2022"
+        $path = "${vs}\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+        if (!(Test-Path($path)))
+        {
+            $path = "${vs}\Professional\VC\Auxiliary\Build\vcvars64.bat"
+        }
+        if (!(Test-Path($path)))
+        {
+            $path = "${vs}\Community\VC\Auxiliary\Build\vcvars64.bat"
+        }
+
+        Write-Host "Use: ${path}" -ForegroundColor Green
+
+        cmd.exe /c "call `"${path}`" && set > %temp%\vcvars.txt"
+        Get-Content "${env:temp}\vcvars.txt" | Foreach-Object {
+            if ($_ -match "^(.*?)=(.*)$") {
+                Set-Content "env:\$($matches[1])" $matches[2]
+            }
+        }
+    }
+    CallVisualStudioDeveloperConsole
+
+    $CMAKE_MSVC_RUNTIME_LIBRARY = "MultiThreaded"
+    if ($Configuration -eq "Debug")
+    {
+        $CMAKE_MSVC_RUNTIME_LIBRARY += "Debug"
+    }
+
+    $cmakeArgs = @(
+        "-D CMAKE_INSTALL_PREFIX=${installDir}"
+        "-D CMAKE_BUILD_TYPE=${Configuration}"
+        "-D BUILD_SHARED_LIBS=$sharedFlag"
+        "-D protobuf_BUILD_TESTS=OFF"
+        "${sourceDir}"
+    )
+}
+elseif ($global:IsMacOS)
+{
+    $cmakeArgs = @(
+        "-D CMAKE_INSTALL_PREFIX=${installDir}"
+        "-D CMAKE_BUILD_TYPE=${Configuration}"
+        "-D BUILD_SHARED_LIBS=$sharedFlag"
+        "-D protobuf_BUILD_TESTS=OFF"
+        "${sourceDir}"
+    )
+}
+elseif ($global:IsLinux)
+{
+    $cmakeArgs = @(
+        "-D CMAKE_INSTALL_PREFIX=${installDir}"
+        "-D CMAKE_BUILD_TYPE=${Configuration}"
+        "-D BUILD_SHARED_LIBS=$sharedFlag"
+        "-D protobuf_BUILD_TESTS=OFF"
+        "${sourceDir}"
+    )
+}
+
+
+$configLogFile = Join-Path $buildDir cmake-config.log
+$buildLogFile = Join-Path $buildDir cmake-build.log
+
+cmake @cmakeArgs 2>&1 | Tee-Object -FilePath $configLogFile
+cmake --build . --config ${Configuration} --target install 2>&1 | Tee-Object -FilePath $buildLogFile
+
+Pop-Location
