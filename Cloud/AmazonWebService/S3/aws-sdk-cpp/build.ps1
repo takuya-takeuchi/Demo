80efa28ff289ec6f5daaa3ed4a0990deef301d71
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2026-04-29 20:51:57 +0900

    fix

--- Cloud/AmazonWebService/S3/aws-sdk-cpp/build.ps1
+++ Cloud/AmazonWebService/S3/aws-sdk-cpp/build.ps1
@@ -11,10 +11,42 @@ Param
    $Configuration
 )
 
-$current = $PSScriptRoot
+$ConfigurationArray =
+@(
+   "Debug",
+   "Release",
+   "RelWithDebInfo",
+   "MinSizeRel"
+)
 
-# update submodule
-git submodule update --init --recursive .
+if ($ConfigurationArray.Contains($Configuration) -eq $False)
+{
+   $candidate = $ConfigurationArray -join "/"
+   Write-Host "Specify build configuration [${candidate}]" -ForegroundColor Red
+   exit -1
+}
+
+$current = $PSScriptRoot
+$configPath = Join-Path $current "build-config.json"
+if (!(Test-Path($configPath)))
+{
+    Write-Host "${configPath} is missing" -ForegroundColor Red
+    exit
+}
+
+$config = Get-Content -Path $configPath | ConvertFrom-Json
+$target = "aws-sdk-cpp"
+$version = $config.awssdkcpp.version
+if ($config.awssdkcpp.shared)
+{
+    $shared = "dynamic"
+    $sharedFlag = "ON"
+}
+else
+{
+    $shared = "static"
+    $sharedFlag = "OFF"
+}
 
 # get os name
 if ($global:IsWindows)
@@ -30,28 +62,119 @@ elseif ($global:IsLinux)
     $os = "linux"
 }
 
-$target = "aws-sdk-cpp"
-
+# build
 $sourceDir = Join-Path $current $target
 $buildDir = Join-Path $current build | `
             Join-Path -ChildPath $os | `
-            Join-Path -ChildPath $target
+            Join-Path -ChildPath $target | `
+            Join-Path -ChildPath $version | `
+            Join-Path -ChildPath $shared | `
+            Join-Path -ChildPath $Configuration
 $installDir = Join-Path $current install | `
               Join-Path -ChildPath $os | `
-              Join-Path -ChildPath $target
-$binDir = Join-Path $installDir bin
-$libDir = Join-Path $installDir lib
-$includeDir = Join-Path $installDir include
+              Join-Path -ChildPath $target | `
+              Join-Path -ChildPath $version | `
+              Join-Path -ChildPath $shared | `
+              Join-Path -ChildPath $Configuration
 
 New-Item -Type Directory $buildDir -Force | Out-Null
 New-Item -Type Directory $installDir -Force | Out-Null
 
+Push-Location $current
+# it takes so long time....
+# git submodule update --init --recursive $sourceDir
+git checkout $sourceDir
+Pop-Location
+
+Push-Location $sourceDir
+git fetch -ap
+git checkout $version
+# it takes so long time....
+# git submodule update --init --recursive .
+Pop-Location
+
+# apply patch
+$patch = Join-Path $current patch |
+         Join-Path -ChildPath cxxopts |
+         Join-Path -ChildPath $version |
+         Join-Path -ChildPath $os
+if (Test-Path($patch))
+{
+    Copy-Item -Recurse $patch/* $sourceDir -Force
+}
+
 Push-Location $buildDir
-cmake -D CMAKE_INSTALL_PREFIX=$installDir `
-      -D CMAKE_BUILD_TYPE=$Configuration `
-      -D BUILD_SHARED_LIBS=OFF `
-      -D ENABLE_TESTING=OFF `
-      -D BUILD_ONLY=s3 `
-      $sourceDir
-cmake --build . --config $Configuration --target install
+
+$cmakeArgs = @()
+if ($global:IsWindows)
+{
+    function CallVisualStudioDeveloperConsole()
+    {
+        $vs = "C:\Program Files\Microsoft Visual Studio\2022"
+        $path = "${vs}\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+        if (!(Test-Path($path)))
+        {
+            $path = "${vs}\Professional\VC\Auxiliary\Build\vcvars64.bat"
+        }
+        if (!(Test-Path($path)))
+        {
+            $path = "${vs}\Community\VC\Auxiliary\Build\vcvars64.bat"
+        }
+
+        Write-Host "Use: ${path}" -ForegroundColor Green
+
+        cmd.exe /c "call `"${path}`" && set > %temp%\vcvars.txt"
+        Get-Content "${env:temp}\vcvars.txt" | Foreach-Object {
+            if ($_ -match "^(.*?)=(.*)$") {
+                Set-Content "env:\$($matches[1])" $matches[2]
+            }
+        }
+    }
+    CallVisualStudioDeveloperConsole
+
+    if ($config.windows.msvcStaticRuntime)
+    {
+        $FORCE_SHARED_CRT = "OFF"
+    }
+    else
+    {
+        $FORCE_SHARED_CRT = "ON"
+    }
+
+    $cmakeArgs += @(
+        "-G", "Visual Studio 17 2022", "-A", "x64", "-T", "host=x64"
+        "-D CMAKE_INSTALL_PREFIX=${installDir}"
+        "-D CMAKE_BUILD_TYPE=${Configuration}"
+        "-D FORCE_SHARED_CRT=${FORCE_SHARED_CRT}"
+    )
+}
+elseif ($global:IsMacOS)
+{
+    $cmakeArgs += @(
+        "-D CMAKE_INSTALL_PREFIX=${installDir}"
+        "-D CMAKE_BUILD_TYPE=${Configuration}"
+    )
+}
+elseif ($global:IsLinux)
+{
+    $cmakeArgs += @(
+        "-D CMAKE_INSTALL_PREFIX=${installDir}"
+        "-D CMAKE_BUILD_TYPE=${Configuration}"
+    )
+}
+
+$cmakeArgs += @(
+    "-D BUILD_SHARED_LIBS=$sharedFlag"
+    "-D BUILD_ONLY=s3"
+    "-D ENABLE_TESTING=OFF"
+    "${sourceDir}"
+)
+
+$configLogFile = Join-Path $buildDir cmake-config.log
+$buildLogFile = Join-Path $buildDir cmake-build.log
+
+cmake @cmakeArgs 2>&1 | Tee-Object -FilePath $configLogFile
+$nproc = [Environment]::ProcessorCount
+cmake --build . --config ${Configuration} --target install --parallel $nproc 2>&1 | Tee-Object -FilePath $buildLogFile
+
 Pop-Location
