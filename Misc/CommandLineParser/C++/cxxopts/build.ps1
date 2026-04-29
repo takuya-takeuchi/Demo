7087b3d1d1238cdcee2a9ec9a21c593688a071ec
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2026-04-29 15:04:53 +0900

    fix

---
+++ Misc/CommandLineParser/C++/cxxopts/build.ps1
@@ -0,0 +1,177 @@
+#***************************************
+#Arguments
+#%1: Build Configuration (Release/Debug)
+#***************************************
+Param
+(
+   [Parameter(
+   Mandatory=$True,
+   Position = 1
+   )][string]
+   $Configuration
+)
+
+$ConfigurationArray =
+@(
+   "Debug",
+   "Release",
+   "RelWithDebInfo",
+   "MinSizeRel"
+)
+
+if ($ConfigurationArray.Contains($Configuration) -eq $False)
+{
+   $candidate = $ConfigurationArray -join "/"
+   Write-Host "Specify build configuration [${candidate}]" -ForegroundColor Red
+   exit -1
+}
+
+$current = $PSScriptRoot
+$configPath = Join-Path $current "build-config.json"
+if (!(Test-Path($configPath)))
+{
+    Write-Host "${configPath} is missing" -ForegroundColor Red
+    exit
+}
+
+$config = Get-Content -Path $configPath | ConvertFrom-Json
+$target = "cxxopts"
+$version = $config.cxxopts.version
+if ($config.cxxopts.shared)
+{
+    $shared = "dynamic"
+    $sharedFlag = $True
+}
+else
+{
+    $shared = "static"
+    $sharedFlag = $False
+}
+
+# get os name
+if ($global:IsWindows)
+{
+    $os = "win"
+}
+elseif ($global:IsMacOS)
+{
+    $os = "osx"
+}
+elseif ($global:IsLinux)
+{
+    $os = "linux"
+}
+
+# build
+$sourceDir = Join-Path $current $target
+$buildDir = Join-Path $current build | `
+            Join-Path -ChildPath $os | `
+            Join-Path -ChildPath $target | `
+            Join-Path -ChildPath $version | `
+            Join-Path -ChildPath $shared | `
+            Join-Path -ChildPath $Configuration
+$installDir = Join-Path $current install | `
+              Join-Path -ChildPath $os | `
+              Join-Path -ChildPath $target | `
+              Join-Path -ChildPath $version | `
+              Join-Path -ChildPath $shared | `
+              Join-Path -ChildPath $Configuration
+
+New-Item -Type Directory $buildDir -Force | Out-Null
+New-Item -Type Directory $installDir -Force | Out-Null
+
+Push-Location $current
+git submodule update --init --recursive $sourceDir
+Pop-Location
+
+Push-Location $sourceDir
+git fetch -ap
+git checkout $version
+git submodule update --init --recursive .
+Pop-Location
+
+# apply patch
+$patch = Join-Path $current patch |
+         Join-Path -ChildPath cxxopts |
+         Join-Path -ChildPath $version |
+         Join-Path -ChildPath $os
+if (Test-Path($patch))
+{
+    Copy-Item -Recurse $patch/* $sourceDir -Force
+}
+
+Push-Location $buildDir
+
+$cmakeArgs = @()
+if ($global:IsWindows)
+{
+    function CallVisualStudioDeveloperConsole()
+    {
+        $vs = "C:\Program Files\Microsoft Visual Studio\2022"
+        $path = "${vs}\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+        if (!(Test-Path($path)))
+        {
+            $path = "${vs}\Professional\VC\Auxiliary\Build\vcvars64.bat"
+        }
+        if (!(Test-Path($path)))
+        {
+            $path = "${vs}\Community\VC\Auxiliary\Build\vcvars64.bat"
+        }
+
+        Write-Host "Use: ${path}" -ForegroundColor Green
+
+        cmd.exe /c "call `"${path}`" && set > %temp%\vcvars.txt"
+        Get-Content "${env:temp}\vcvars.txt" | Foreach-Object {
+            if ($_ -match "^(.*?)=(.*)$") {
+                Set-Content "env:\$($matches[1])" $matches[2]
+            }
+        }
+    }
+    CallVisualStudioDeveloperConsole
+
+    if ($config.windows.msvcStaticRuntime)
+    {
+        $CMAKE_MSVC_RUNTIME_LIBRARY = "MultiThreaded$<$<CONFIG:Debug>:Debug>"
+    }
+    else
+    {
+        $CMAKE_MSVC_RUNTIME_LIBRARY = "MultiThreaded$<$<CONFIG:Debug>:Debug>DLL"
+    }
+
+    $cmakeArgs += @(
+        "-G", "Visual Studio 17 2022", "-A", "x64", "-T", "host=x64"
+        "-D CMAKE_INSTALL_PREFIX=${installDir}"
+        "-D CMAKE_BUILD_TYPE=${Configuration}"
+        "-D CMAKE_MSVC_RUNTIME_LIBRARY=${CMAKE_MSVC_RUNTIME_LIBRARY}"
+    )
+}
+elseif ($global:IsMacOS)
+{
+    $cmakeArgs += @(
+        "-D CMAKE_INSTALL_PREFIX=${installDir}"
+        "-D CMAKE_BUILD_TYPE=${Configuration}"
+    )
+}
+elseif ($global:IsLinux)
+{
+    $cmakeArgs += @(
+        "-D CMAKE_INSTALL_PREFIX=${installDir}"
+        "-D CMAKE_BUILD_TYPE=${Configuration}"
+    )
+}
+
+$cmakeArgs += @(
+    "-D CXXOPTS_BUILD_EXAMPLES=OFF"
+    "-D CXXOPTS_BUILD_TESTS=OFF"
+    "-D CXXOPTS_USE_UNICODE_HELP=OFF"
+    "${sourceDir}"
+)
+
+$configLogFile = Join-Path $buildDir cmake-config.log
+$buildLogFile = Join-Path $buildDir cmake-build.log
+
+cmake @cmakeArgs 2>&1 | Tee-Object -FilePath $configLogFile
+$nproc = [Environment]::ProcessorCount
+cmake --build . --config ${Configuration} --target install --parallel $nproc 2>&1 | Tee-Object -FilePath $buildLogFile
+
+Pop-Location
