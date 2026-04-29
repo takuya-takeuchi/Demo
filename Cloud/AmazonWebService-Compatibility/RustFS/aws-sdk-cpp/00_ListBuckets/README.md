eb433bf1d98b89a880c86722a6a94025dc0ef323
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2026-04-30 01:03:44 +0900

    fix

---
+++ Cloud/AmazonWebService-Compatibility/RustFS/aws-sdk-cpp/00_ListBuckets/README.md
@@ -0,0 +1,69 @@
+# ListBuckets
+
+## Abstracts
+
+* Enumerate backets
+
+## Requirements
+
+### Common
+
+* Powershell
+* CMake 3.15.0 or later
+* C++ Compiler supports C++11
+
+### Windows
+
+* Visual Studio
+
+### Ubuntu
+
+* g++
+
+### OSX
+
+* Xcode
+
+## Dependencies
+
+* [AWS SDK for C++](https://github.com/aws/aws-sdk-cpp)
+  * 1.11.798
+  * Apache 2.0 License
+
+## How to build?
+
+### Build AWS SDK for C++
+
+Go to [AmazonWebService-Compatibility](../../..).
+
+````shell
+$ pwsh build.ps1 <Debug/Release>
+````
+
+Once time you built AWS SDK for C++, you need not to do again.
+
+### Build
+
+````shell
+$ pwsh build.ps1 <Debug/Release>
+````
+
+Then, program will be present in `install/<your os name>/bin`.
+
+## How to use?
+
+<img src="./images/image.png" />
+
+### Windows
+
+````bat
+$ set AWS_ACCESS_KEY=rustfsadmin
+$ set AWS_SECRET_ACCESS_KEY=rustfsadmin
+$ .\install\win\Release\bin\Demo.exe http://192.168.11.45:9000 ap-northeast-1
+[Info]    endpoint: http://192.168.11.45:9000
+[Info]      region: ap-northeast-1
+[Info] Aws::InitAPI
+[Info] Use DefaultAWSCredentialsProviderChain like IAM Role etc
+[Error] while ListBuckets InvalidAccessKeyId The Access Key Id you provided does not exist in our records.
+[Info] Aws::ShutdownAPI
+````
---
+++ Cloud/AmazonWebService-Compatibility/RustFS/aws-sdk-cpp/00_ListBuckets/build.ps1
@@ -0,0 +1,171 @@
+#***************************************
+#Arguments
+#%1: Build Configuration (Release/Debug)
+#***************************************
+Param
+(
+   [Parameter(
+   Mandatory=$True,
+   Position = 1
+   )][string]
+   $Configuration
+)
+
+$current = $PSScriptRoot
+$rootDir = Split-Path $current -Parent
+$rootDir = Split-Path $rootDir -Parent
+$rootDir = Split-Path $rootDir -Parent
+$configPath = Join-Path $rootDir "build-config.json"
+if (!(Test-Path($configPath)))
+{
+    Write-Host "${configPath} is missing" -ForegroundColor Red
+    exit
+}
+
+$config = Get-Content -Path $configPath | ConvertFrom-Json
+
+# get os name
+if ($global:IsWindows)
+{
+    $os = "win"
+}
+elseif ($global:IsMacOS)
+{
+    $os = "osx"
+}
+elseif ($global:IsLinux)
+{
+    $os = "linux"
+}
+
+$awsSdkCppVersion = $config.awssdkcpp.version
+if ($config.awssdkcpp.shared)
+{
+    $awsSdkCppShared = "dynamic"
+}
+else
+{
+    $awsSdkCppShared = "static"
+}
+
+# get os name
+if ($global:IsWindows)
+{
+    $os = "win"
+}
+elseif ($global:IsMacOS)
+{
+    $os = "osx"
+}
+elseif ($global:IsLinux)
+{
+    $os = "linux"
+}
+
+# build
+$sourceDir = $current
+$buildDir = Join-Path $current build | `
+            Join-Path -ChildPath $os | `
+            Join-Path -ChildPath $version | `
+            Join-Path -ChildPath $Configuration
+$installDir = Join-Path $current install | `
+              Join-Path -ChildPath $os | `
+              Join-Path -ChildPath $version | `
+              Join-Path -ChildPath $shared | `
+              Join-Path -ChildPath $Configuration
+
+$AWSSDKCPP_INSTALL_DIR = Join-Path $rootDir install | `
+                         Join-Path -ChildPath $os | `
+                         Join-Path -ChildPath aws-sdk-cpp | `
+                         Join-Path -ChildPath $awsSdkCppVersion | `
+                         Join-Path -ChildPath $awsSdkCppShared | `
+                         Join-Path -ChildPath $Configuration
+$AWSSDKCPP_CMAKE_DIR = Join-Path $AWSSDKCPP_INSTALL_DIR lib | `
+                       Join-Path -ChildPath cmake | `
+                       Join-Path -ChildPath AWSSDK
+$AWSSDKCPP_CMAKE_ROOT_DIR = (Get-ChildItem -Path $AWSSDKCPP_INSTALL_DIR -Recurse -Directory | Where-Object { $_.Name -eq "cmake" } | Select-Object -First 1).FullName
+$AWSSDKCPP_CMAKE_FILE_DIRS = Get-ChildItem -Path $AWSSDKCPP_CMAKE_ROOT_DIR -Recurse -Directory | Where-Object { $_.Name -match "^aws-" }
+$AWSSDKCPP_CMAKE_FILE_DIRS += $AWSSDKCPP_CMAKE_DIR
+
+New-Item -Type Directory $buildDir -Force | Out-Null
+New-Item -Type Directory $installDir -Force | Out-Null
+
+Push-Location $buildDir
+
+$cmakeArgs = @()
+if ($global:IsWindows)
+{
+    function CallVisualStudioDeveloperConsole()
+    {
+        $vs = "C:\Program Files\Microsoft Visual Studio\2022"
+        $path = "${vs}\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+        if (!(Test-Path($path)))
+        {
+            $path = "${vs}\Professional\VC\Auxiliary\Build\vcvars64.bat"
+        }
+        if (!(Test-Path($path)))
+        {
+            $path = "${vs}\Community\VC\Auxiliary\Build\vcvars64.bat"
+        }
+
+        Write-Host "Use: ${path}" -ForegroundColor Green
+
+        cmd.exe /c "call `"${path}`" && set > %temp%\vcvars.txt"
+        Get-Content "${env:temp}\vcvars.txt" | Foreach-Object {
+            if ($_ -match "^(.*?)=(.*)$") {
+                Set-Content "env:\$($matches[1])" $matches[2]
+            }
+        }
+    }
+    CallVisualStudioDeveloperConsole
+    chcp 65001
+
+    if ($config.windows.msvcStaticRuntime)
+    {
+        $CMAKE_MSVC_RUNTIME_LIBRARY = "MultiThreaded"
+    }
+    else
+    {
+        $CMAKE_MSVC_RUNTIME_LIBRARY = "MultiThreaded$<$<CONFIG:Debug>:Debug>DLL"
+    }
+
+    $CMAKE_PREFIX_PATH = $AWSSDKCPP_CMAKE_FILE_DIRS -Join ";"
+    $cmakeArgs += @(
+        "-G", "Visual Studio 17 2022", "-A", "x64", "-T", "host=x64"
+        "-D CMAKE_INSTALL_PREFIX=${installDir}"
+        "-D CMAKE_BUILD_TYPE=${Configuration}"
+        "-D CMAKE_MSVC_RUNTIME_LIBRARY=${CMAKE_MSVC_RUNTIME_LIBRARY}"
+        "-D CMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}"
+    )
+}
+elseif ($global:IsMacOS)
+{
+    $CMAKE_PREFIX_PATH = $AWSSDKCPP_CMAKE_FILE_DIRS -Join ":"
+    $cmakeArgs += @(
+        "-D CMAKE_INSTALL_PREFIX=${installDir}"
+        "-D CMAKE_BUILD_TYPE=${Configuration}"
+        "-D CMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}"
+    )
+}
+elseif ($global:IsLinux)
+{
+    $CMAKE_PREFIX_PATH = $AWSSDKCPP_CMAKE_FILE_DIRS -Join ":"
+    $cmakeArgs += @(
+        "-D CMAKE_INSTALL_PREFIX=${installDir}"
+        "-D CMAKE_BUILD_TYPE=${Configuration}"
+        "-D CMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}"
+    )
+}
+
+$cmakeArgs += @(
+    "${sourceDir}"
+)
+
+$configLogFile = Join-Path $buildDir cmake-config.log
+$buildLogFile = Join-Path $buildDir cmake-build.log
+
+cmake @cmakeArgs 2>&1 | Tee-Object -FilePath $configLogFile
+$nproc = [Environment]::ProcessorCount
+cmake --build . --config ${Configuration} --target install --parallel $nproc 2>&1 | Tee-Object -FilePath $buildLogFile
+
+Pop-Location
