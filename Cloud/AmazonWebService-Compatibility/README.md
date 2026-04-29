1a0a6ad3824271844c740cb4617f868842f78a1f
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2026-04-29 23:04:51 +0900

    fix

---
+++ Cloud/AmazonWebService-Compatibility/README.md
@@ -0,0 +1,45 @@
+# Amazon Web Service (Compatibility)
+
+## Abstracts
+
+* Build aws-sdk-cpp
+  * This artifact are used in subdirectory
+
+## Requirements
+
+### Common
+
+* Powershell
+* CMake 3.15.0 or later
+* C++ Compiler
+
+### Windows
+
+* Visual Studio
+
+### Ubuntu
+
+* g++
+* libcurl4-openssl-dev
+  * via `apt` command
+
+### OSX
+
+* Xcode
+
+## Dependencies
+
+* [AWS SDK for C++](https://github.com/aws/aws-sdk-cpp)
+  * 1.11.798
+  * Apache 2.0 License
+
+## How to use?
+
+At first, you must build [AWS SDK for C++](https://github.com/aws/aws-sdk-cpp).
+
+````shell
+$ pwsh build.ps1 <Debug/Release>
+````
+
+You can change modules you want to use by edit [build-config.json](./build-config.json).
+For example,  change `{ "option": "ec2",  "flag": false },` to `{ "option": "ec2", "flag": true },`, `aws-cpp-sdk-ec2` module will be generated.
---
+++ Cloud/AmazonWebService/build.ps1
@@ -0,0 +1,186 @@
+#***************************************
+#Arguments
+#%1: Build Configuration (Release/Debug)
+#***************************************
+Param
+(
+   [Parameter(
+   Mandatory=$True,
+   Position = 1
+   )][string]
+   $Configuration
+)
+
+$ConfigurationArray =
+@(
+   "Debug",
+   "Release",
+   "RelWithDebInfo",
+   "MinSizeRel"
+)
+
+if ($ConfigurationArray.Contains($Configuration) -eq $False)
+{
+   $candidate = $ConfigurationArray -join "/"
+   Write-Host "Specify build configuration [${candidate}]" -ForegroundColor Red
+   exit -1
+}
+
+$current = $PSScriptRoot
+$configPath = Join-Path $current "build-config.json"
+if (!(Test-Path($configPath)))
+{
+    Write-Host "${configPath} is missing" -ForegroundColor Red
+    exit
+}
+
+$config = Get-Content -Path $configPath | ConvertFrom-Json
+$target = "aws-sdk-cpp"
+$version = $config.awssdkcpp.version
+if ($config.awssdkcpp.shared)
+{
+    $shared = "dynamic"
+    $sharedFlag = "ON"
+}
+else
+{
+    $shared = "static"
+    $sharedFlag = "OFF"
+}
+
+# get os name
+if ($global:IsWindows)
+{
+    $os = "win"
+}
+elseif ($global:IsMacOS)
+{
+    $os = "osx"
+}
+elseif ($global:IsLinux)
+{
+    $os = "linux"
+}
+
+# build
+$sourceDir = Join-Path $current $target
+$buildDir = Join-Path $current build | `
+            Join-Path -ChildPath $os | `
+            Join-Path -ChildPath $target | `
+            Join-Path -ChildPath $version | `
+            Join-Path -ChildPath $shared | `
+            Join-Path -ChildPath $Configuration
+$installDir = Join-Path $current install | `
+              Join-Path -ChildPath $os | `
+              Join-Path -ChildPath $target | `
+              Join-Path -ChildPath $version | `
+              Join-Path -ChildPath $shared | `
+              Join-Path -ChildPath $Configuration
+
+New-Item -Type Directory $buildDir -Force | Out-Null
+New-Item -Type Directory $installDir -Force | Out-Null
+
+Push-Location $current
+# it may be deleted
+git checkout $sourceDir
+Pop-Location
+
+Push-Location $sourceDir
+git fetch -ap
+git checkout $version
+# it takes so long time....
+git submodule update --init --recursive .
+Pop-Location
+
+# apply patch
+$patch = Join-Path $current patch |
+         Join-Path -ChildPath cxxopts |
+         Join-Path -ChildPath $version |
+         Join-Path -ChildPath $os
+if (Test-Path($patch))
+{
+    Copy-Item -Recurse $patch/* $sourceDir -Force
+}
+
+Push-Location $buildDir
+
+$cmakeArgs = @()
+if ($global:IsWindows)
+{
+    function CallVisualStudioDeveloperConsole()
+    {
+        $vs = "C:\Program Files\Microsoft Visual Studio\2022"
+        $path = "${vs}\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+        if (!(Test-Path($path)))
+        {
+            $path = "${vs}\Professional\VC\Auxiliary\Build\vcvars64.bat"
+        }
+        if (!(Test-Path($path)))
+        {
+            $path = "${vs}\Community\VC\Auxiliary\Build\vcvars64.bat"
+        }
+
+        Write-Host "Use: ${path}" -ForegroundColor Green
+
+        cmd.exe /c "call `"${path}`" && set > %temp%\vcvars.txt"
+        Get-Content "${env:temp}\vcvars.txt" | Foreach-Object {
+            if ($_ -match "^(.*?)=(.*)$") {
+                Set-Content "env:\$($matches[1])" $matches[2]
+            }
+        }
+    }
+    CallVisualStudioDeveloperConsole
+
+    if ($config.windows.msvcStaticRuntime)
+    {
+        $FORCE_SHARED_CRT = "OFF"
+    }
+    else
+    {
+        $FORCE_SHARED_CRT = "ON"
+    }
+
+    $cmakeArgs += @(
+        "-G", "Visual Studio 17 2022", "-A", "x64", "-T", "host=x64"
+        "-D CMAKE_INSTALL_PREFIX=${installDir}"
+        "-D CMAKE_BUILD_TYPE=${Configuration}"
+        "-D FORCE_SHARED_CRT=${FORCE_SHARED_CRT}"
+    )
+}
+elseif ($global:IsMacOS)
+{
+    $cmakeArgs += @(
+        "-D CMAKE_INSTALL_PREFIX=${installDir}"
+        "-D CMAKE_BUILD_TYPE=${Configuration}"
+    )
+}
+elseif ($global:IsLinux)
+{
+    $cmakeArgs += @(
+        "-D CMAKE_INSTALL_PREFIX=${installDir}"
+        "-D CMAKE_BUILD_TYPE=${Configuration}"
+    )
+}
+
+$buildOnlys = @()
+$config.awssdkcpp.build | Where-Object { $_.flag } | ForEach-Object {
+    $buildOnlys += $_.option
+}
+
+$buildOnly = $buildOnlys -Join ";"
+Write-Host "Build only: ${buildOnly}" -ForegroundColor Green
+$cmakeArgs += @(
+    "-D BUILD_SHARED_LIBS=$sharedFlag"
+    "-D BUILD_ONLY=${buildOnly}"
+    "-D ENABLE_TESTING=OFF"
+    "${sourceDir}"
+)
+
+$configLogFile = Join-Path $buildDir cmake-config.log
+$buildLogFile = Join-Path $buildDir cmake-build.log
+
+cmake @cmakeArgs 2>&1 | Tee-Object -FilePath $configLogFile
+$nproc = [Environment]::ProcessorCount
+cmake --build . --config ${Configuration} --target install --parallel $nproc 2>&1 | Tee-Object -FilePath $buildLogFile
+
+Pop-Location
