f5611f955408ee5ab10df1ee15d6c63ab9cdac25
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2026-05-10 18:01:38 +0900

    fix

---
+++ Cloud/AmazonWebService/S3/aws-sdk-cpp/03_GetObject/README.md
@@ -0,0 +1,139 @@
+# Get File from S3
+
+## Abstracts
+
+* Download file from S3
+
+## Requirements
+
+### Common
+
+* Powershell
+* CMake 3.15.0 or later
+* C++ Compiler supports C++11
+
+### Windows
+
+* Visual Studio
+
+### Ubuntu
+
+* g++
+
+### OSX
+
+* Xcode
+
+## Dependencies
+
+* [AWS SDK for C++](https://github.com/aws/aws-sdk-cpp)
+  * 1.11.798
+  * Apache 2.0 License
+
+## How to build?
+
+### Build AWS SDK for C++
+
+Go to [AmazonWebService](../../..).
+
+````shell
+$ pwsh build.ps1 <Debug/Release>
+````
+
+Once time you built AWS SDK for C++, you need not to do again.
+
+### Build
+
+````shell
+$ pwsh build.ps1 <Debug/Release>
+````
+
+Then, program will be present in `install/<your os name>/bin`.
+
+## How to use?
+
+### Windows
+
+````bat
+$ set AWS_ACCESS_KEY_ID=xxxxxxxx
+$ set AWS_SECRET_ACCESS_KEY=yyyyyyyy
+$ .\install\win\Release\bin\Demo.exe http://192.168.11.45:9000 data-bucket /tmp/test-image.jpg ap-northeast-1
+[Info]    endpoint: http://192.168.11.45:9000
+[Info] bucket_name: data-bucket
+[Info] object_name: /tmp/test-image.jpg
+[Info]      region: ap-northeast-1
+[Info]    filepath: lenna.jpg
+[Info] Aws::InitAPI
+[Info] Use access key and secret key
+[Info] Succeded to upload file
+[Info] Aws::ShutdownAPI
+````
+
+### Linux
+
+````bash
+$ export AWS_ACCESS_KEY_ID=xxxxxxxx
+$ export AWS_SECRET_ACCESS_KEY=yyyyyyyy
+$ ./install/linux/Release/bin/Demo ve8rjmzuy84wq7f2iyhp lenna.jpg ap-northeast-1
+bucket_name: ve8rjmzuy84wq7f2iyhp
+object_name: lenna.jpg
+     region: ap-northeast-1
+[Info] Aws::InitAPI
+[Info] GetObject
+[Info] Retrieved object 'lenna.jpg' from bucket 've8rjmzuy84wq7f2iyhp'.
+[Info] Aws::ShutdownAPI
+````
+
+### OSX
+
+````bash
+$ export AWS_ACCESS_KEY_ID=xxxxxxxx
+$ export AWS_SECRET_ACCESS_KEY=yyyyyyyy
+$ ./install/osx/Release/bin/Demo http://192.168.11.45:9000 data-bucket /tmp/test-image.jpg ap-northeast-1 lenna.jpg
+[Info]    endpoint: http://192.168.11.45:9000
+[Info] bucket_name: data-bucket
+[Info] object_name: /tmp/test-image.jpg
+[Info]      region: ap-northeast-1
+[Info]    filepath: lenna.jpg
+[Info] Aws::InitAPI
+[Info] Use access key and secret key
+[Info] Succeded to upload file
+[Info] Aws::ShutdownAPI
+````
+
+## Why does program not work?
+
+### Error: GetObject: Access Denied
+
+You must check the following things
+
+#### Block public access (bucket setting)
+
+This program does not take care of credentials. In other words, we have to disable block public access in AWS console.
+
+<img src="images/block.png" />
+
+#### Block policy
+
+Bcket policy must allow write actions.
+For examples, you can write json like 
+
+````json
+{
+    "Version": "2012-10-17",
+    "Id": "Policy9999999999999",
+    "Statement": [
+        {
+            "Sid": "Stmt9999999999999",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:DeleteObject",
+                "s3:GetObject",
+                "s3:PutObject"
+            ],
+            "Resource": "arn:aws:s3:::<your-bucket-name>/*"
+        }
+    ]
+}
+````
---
+++ Cloud/AmazonWebService/S3/aws-sdk-cpp/03_GetObject/build.ps1
@@ -0,0 +1,183 @@
+#***************************************
+#Arguments
+#%1: Build Configuration (Release/Debug)
+#***************************************
+Param
+(
+   [Parameter(
+   Mandatory=$True,
+   Position = 1
+   )][string]
+   $Configuration
+)
+
+$ConfigurationArray =
+@(
+   "Debug",
+   "Release",
+   "RelWithDebInfo",
+   "MinSizeRel"
+)
+
+if ($ConfigurationArray.Contains($Configuration) -eq $False)
+{
+   $candidate = $ConfigurationArray -join "/"
+   Write-Host "Specify build configuration [${candidate}]" -ForegroundColor Red
+   exit -1
+}
+
+$current = $PSScriptRoot
+$rootDir = Split-Path $current -Parent
+$rootDir = Split-Path $rootDir -Parent
+$rootDir = Split-Path $rootDir -Parent
+$configPath = Join-Path $rootDir "build-config.json"
+if (!(Test-Path($configPath)))
+{
+    Write-Host "${configPath} is missing" -ForegroundColor Red
+    exit
+}
+
+$config = Get-Content -Path $configPath | ConvertFrom-Json
+
+# get os name
+if ($global:IsWindows)
+{
+    $os = "win"
+}
+elseif ($global:IsMacOS)
+{
+    $os = "osx"
+}
+elseif ($global:IsLinux)
+{
+    $os = "linux"
+}
+
+$awsSdkCppVersion = $config.awssdkcpp.version
+if ($config.awssdkcpp.shared)
+{
+    $awsSdkCppShared = "dynamic"
+}
+else
+{
+    $awsSdkCppShared = "static"
+}
+
+# get os name
+if ($global:IsWindows)
+{
+    $os = "win"
+}
+elseif ($global:IsMacOS)
+{
+    $os = "osx"
+}
+elseif ($global:IsLinux)
+{
+    $os = "linux"
+}
+
+# build
+$sourceDir = $current
+$buildDir = Join-Path $current build | `
+            Join-Path -ChildPath $os | `
+            Join-Path -ChildPath $version | `
+            Join-Path -ChildPath $Configuration
+$installDir = Join-Path $current install | `
+              Join-Path -ChildPath $os | `
+              Join-Path -ChildPath $version | `
+              Join-Path -ChildPath $shared | `
+              Join-Path -ChildPath $Configuration
+
+$AWSSDKCPP_INSTALL_DIR = Join-Path $rootDir install | `
+                         Join-Path -ChildPath $os | `
+                         Join-Path -ChildPath aws-sdk-cpp | `
+                         Join-Path -ChildPath $awsSdkCppVersion | `
+                         Join-Path -ChildPath $awsSdkCppShared | `
+                         Join-Path -ChildPath $Configuration
+$AWSSDKCPP_CMAKE_DIR = Join-Path $AWSSDKCPP_INSTALL_DIR lib | `
+                       Join-Path -ChildPath cmake | `
+                       Join-Path -ChildPath AWSSDK
+
+New-Item -Type Directory $buildDir -Force | Out-Null
+New-Item -Type Directory $installDir -Force | Out-Null
+
+Push-Location $buildDir
+
+$cmakeArgs = @()
+if ($global:IsWindows)
+{
+    function CallVisualStudioDeveloperConsole()
+    {
+        $vs = "C:\Program Files\Microsoft Visual Studio\2022"
+        $path = "${vs}\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+        if (!(Test-Path($path)))
+        {
+            $path = "${vs}\Professional\VC\Auxiliary\Build\vcvars64.bat"
+        }
+        if (!(Test-Path($path)))
+        {
+            $path = "${vs}\Community\VC\Auxiliary\Build\vcvars64.bat"
+        }
+
+        Write-Host "Use: ${path}" -ForegroundColor Green
+
+        cmd.exe /c "call `"${path}`" && set > %temp%\vcvars.txt"
+        Get-Content "${env:temp}\vcvars.txt" | Foreach-Object {
+            if ($_ -match "^(.*?)=(.*)$") {
+                Set-Content "env:\$($matches[1])" $matches[2]
+            }
+        }
+    }
+    CallVisualStudioDeveloperConsole
+    chcp 65001
+
+    if ($config.windows.msvcStaticRuntime)
+    {
+        $CMAKE_MSVC_RUNTIME_LIBRARY = "MultiThreaded$<$<CONFIG:Debug>:Debug>"
+    }
+    else
+    {
+        $CMAKE_MSVC_RUNTIME_LIBRARY = "MultiThreaded$<$<CONFIG:Debug>:Debug>DLL"
+    }
+
+    $cmakeArgs += @(
+        "-G", "Visual Studio 17 2022", "-A", "x64", "-T", "host=x64"
+        "-D CMAKE_INSTALL_PREFIX=${installDir}"
+        "-D CMAKE_BUILD_TYPE=${Configuration}"
+        "-D CMAKE_MSVC_RUNTIME_LIBRARY=${CMAKE_MSVC_RUNTIME_LIBRARY}"
+        "-D CMAKE_PREFIX_PATH=${AWSSDKCPP_INSTALL_DIR}"
+        "-D AWSSDK_DIR=${AWSSDKCPP_CMAKE_DIR}"
+    )
+}
+elseif ($global:IsMacOS)
+{
+    $cmakeArgs += @(
+        "-D CMAKE_INSTALL_PREFIX=${installDir}"
+        "-D CMAKE_BUILD_TYPE=${Configuration}"
+        "-D CMAKE_PREFIX_PATH=${AWSSDKCPP_INSTALL_DIR}"
+        "-D AWSSDK_DIR=${AWSSDKCPP_CMAKE_DIR}"
+    )
+}
+elseif ($global:IsLinux)
+{    
+    $cmakeArgs += @(
+        "-D CMAKE_INSTALL_PREFIX=${installDir}"
+        "-D CMAKE_BUILD_TYPE=${Configuration}"
+        "-D CMAKE_PREFIX_PATH=${AWSSDKCPP_INSTALL_DIR}"
+        "-D AWSSDK_DIR=${AWSSDKCPP_CMAKE_DIR}"
+    )
+}
+
+$cmakeArgs += @(
+    "${sourceDir}"
+)
+
+$configLogFile = Join-Path $buildDir cmake-config.log
+$buildLogFile = Join-Path $buildDir cmake-build.log
+
+cmake @cmakeArgs 2>&1 | Tee-Object -FilePath $configLogFile
+$nproc = [Environment]::ProcessorCount
+cmake --build . --config ${Configuration} --target install --parallel $nproc 2>&1 | Tee-Object -FilePath $buildLogFile
+
+Pop-Location
