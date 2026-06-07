13dc45ea9a3baaf5b05ad14b7e86abc15bf6f152
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2026-06-07 22:13:49 +0900

    Added example to explain gdb creating back trace (#359)

---
+++ DevTools/gdb/01_ShowBackTrace/main.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<int> vec = {1, 2, 3, 4, 5};
+    for (size_t i = 0; ; ++i)
+        std::cout << vec[i] << std::endl;
+
+    return 0;
+}
