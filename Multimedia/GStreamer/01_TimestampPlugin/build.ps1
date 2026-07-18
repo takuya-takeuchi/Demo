e1aec31156bdbeb902eeb0154e9a647f746c26fd
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2026-07-18 11:16:16 +0900

    fix

--- Multimedia/GStreamer/01_TimestampPlugin/build.ps1
+++ Multimedia/GStreamer/01_TimestampPlugin/build.ps1
@@ -11,6 +11,21 @@ Param
    $Configuration
 )
 
+function Join-PathArray {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [string[]]$PathElements
+    )
+
+    process {
+        if ($PathElements.Count -eq 0) { return }
+        $result = $PathElements[0]
+        for ($i = 1; $i -lt $PathElements.Count; $i++) { $result = Join-Path -Path $result -ChildPath $PathElements[$i] }
+        return $result
+    }
+}
+
 $current = $PSScriptRoot
 $rootDir = Split-Path $current -Parent
 $configPath = Join-Path $rootDir "build-config.json"
@@ -41,18 +56,10 @@ $version = $config.gstreamer.version
 
 # build
 $sourceDir = $current
-$buildDir = Join-Path $current build | `
-            Join-Path -ChildPath $os | `
-            Join-Path -ChildPath program | `
-            Join-Path -ChildPath $Configuration
-$installDir = Join-Path $current install | `
-              Join-Path -ChildPath $os
-$installBinaryDir = Join-Path $installDir bin
-$targetInstallDir = Join-Path $rootDir install | `
-                    Join-Path -ChildPath $os | `
-                    Join-Path -ChildPath $target | `
-                    Join-Path -ChildPath $version | `
-                    Join-Path -ChildPath $Configuration
+$buildDir = Join-PathArray -PathElements @($current, "build", $os, $target, "program", $Configuration)
+$installDir = Join-PathArray -PathElements @($current, "install", $os)
+$installBinaryDir = Join-PathArray -PathElements @($installDir, "bin")
+$targetInstallDir = Join-PathArray -PathElements @($rootDir, "install", $os, $target, $version, $Configuration)
 if (!(Test-Path(${targetInstallDir})))
 {
     Write-Host "[Error] ${targetInstallDir} is missing" -ForegroundColor Red
@@ -67,11 +74,7 @@ New-Item -Type Directory $installBinaryDir -Force | Out-Null
 Push-Location $buildDir
 if ($global:IsWindows)
 {
-    $pkgConfigExe = Join-Path $rootDir install | `
-                        Join-Path -ChildPath $os | `
-                        Join-Path -ChildPath pkg-config | `
-                        Join-Path -ChildPath bin | `
-                        Join-Path -ChildPath pkg-config.exe
+    $pkgConfigExe = Join-PathArray -PathElements @($rootDir, "install", $os, "pkg-config", "bin", "pkg-config.exe")
     if (!(Test-Path(${pkgConfigExe})))
     {
         Write-Host "[Error] ${pkgConfigExe} is missing. Please run ../download-pkg-config.ps1" -ForegroundColor Red
