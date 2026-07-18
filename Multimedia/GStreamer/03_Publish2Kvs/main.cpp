3f7a7ab0aab3c8c1829eb37545e9d26c7044f15a
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2026-07-18 18:19:54 +0900

    fix

--- Multimedia/GStreamer/03_Publish2Kvs/main.cpp
+++ Multimedia/GStreamer/03_Publish2Kvs/main.cpp
@@ -2,10 +2,12 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
     // 1. Argument validation
     // Expected arguments: [executable] [file_path] [stream_name]
-    if (argc < 3) {
+    if (argc < 3)
+    {
         std::cerr << "Usage: " << argv[0] << " <file_path> <stream_name>" << std::endl;
         return -1;
     }
@@ -17,28 +19,36 @@ int main(int argc, char* argv[]) {
     gst_init(&argc, &argv);
 
     // 3. Construct the pipeline string
-    // Pipeline logic:
-    // filesrc: Read the file
-    // decodebin: Automatically detect and decode the video format
-    // videoconvert: Convert raw video to a format suitable for encoding
-    // x264enc: Encode video to H.264 (using zerolatency tuning for streaming)
-    // h264parse: Format the H.264 stream correctly for the sink
-    // kvssink: The KVS plugin that handles uploading to AWS
-    // fakesink: A dummy sink to complete the pipeline (since kvssink is the actual destination)
-    std::string pipelineStr = 
-        "filesrc location=\"" + filePath + "\" ! "
-        "decodebin ! "
-        "videoconvert ! "
-        "openh264enc rate-control=1 bitrate=2000 ! "
-        "h264parse ! "
-        "kvssink stream-name=\"" + streamName + "\"";
+    std::string pipelineStr = "filesrc location=\"" + filePath +
+                              "\" ! "
+                              "qtdemux name=demux "
+                              "demux.video_0 ! queue ! "
+                              "h264parse ! "
+                              "openh264dec ! "
+                              "videoconvert ! "
+                              "video/x-raw,format=I420 ! "
+                              "openh264enc "
+                              "rate-control=bitrate "
+                              "bitrate=2000000 "
+                              "gop-size=60 ! "
+                              "h264parse config-interval=-1 ! "
+                              "video/x-h264,stream-format=avc,alignment=au ! "
+                              "identity sync=true ! "
+                              "kvssink "
+                              "stream-name=\"" +
+                              streamName +
+                              "\" "
+                              "log-config=\"./kvs_log_configuration\" "
+                              "framerate=30 "
+                              "fragment-duration=2000";
 
     std::cout << "Launching pipeline: " << pipelineStr << std::endl;
 
     GError* error = nullptr;
     GstElement* pipeline = gst_parse_launch(pipelineStr.c_str(), &error);
 
-    if (error) {
+    if (error)
+    {
         std::cerr << "Failed to create pipeline: " << error->message << std::endl;
         g_error_free(error);
         return -1;
@@ -50,18 +60,21 @@ int main(int argc, char* argv[]) {
 
     // 5. Wait for messages on the bus (Errors or End-of-Stream)
     GstBus* bus = gst_element_get_bus(pipeline);
-    GstMessage* msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE,
-        (GstMessageType)(GST_MESSAGE_ERROR | GST_MESSAGE_EOS));
+    GstMessage* msg =
+        gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE, (GstMessageType)(GST_MESSAGE_ERROR | GST_MESSAGE_EOS));
 
     // 6. Handle the message
-    if (msg != nullptr) {
-        if (msg->type == GST_MESSAGE_ERROR) {
+    if (msg != nullptr)
+    {
+        if (msg->type == GST_MESSAGE_ERROR)
+        {
             GError* err;
             gchar* debug_info;
             gst_message_parse_error(msg, &err, &debug_info);
-            
+
             std::cerr << "Streaming Error: " << err->message << std::endl;
-            if (debug_info) {
+            if (debug_info)
+            {
                 std::cerr << "Debug info: " << debug_info << std::endl;
                 g_free(debug_info);
             }
