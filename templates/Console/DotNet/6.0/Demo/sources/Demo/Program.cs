1bd26b38c10c54fe4d418e21a9bd0b62ed48b7bf
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2026-07-27 01:29:52 +0900

    fix

---
+++ templates/Console/DotNet/6.0/Demo/sources/Demo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace Demo
+{
+
+    internal sealed class Program
+    {
+
+        #region Fields
+
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        #endregion
+
+        #region Methods
+
+        private static void Main(string[] args)
+        {
+            Logger.Info("Start");
+        }
+
+        #region Event Handlers
+        #endregion
+
+        #region Helpers
+        #endregion
+
+        #endregion
+
+    }
+
+}
